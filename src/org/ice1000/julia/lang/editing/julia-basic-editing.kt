bbea43f765ab636ad0c542713bfccfbb32dcae81
ice1000 <ice1000kotlin@foxmail.com>
2018-01-26 00:49:30 +0800

    [ move ] Move class

--- src/org/ice1000/julia/lang/editing/julia-basic-editing.kt
+++ src/org/ice1000/julia/lang/editing/julia-basic-editing.kt
@@ -2,11 +2,3 @@ package org.ice1000.julia.lang.editing
 
 import com.intellij.lang.Commenter
 import org.ice1000.julia.lang.*
-
-class JuliaCommenter : Commenter {
-	override fun getCommentedBlockCommentPrefix() = blockCommentPrefix
-	override fun getCommentedBlockCommentSuffix() = blockCommentSuffix
-	override fun getBlockCommentPrefix() = JULIA_DOC_SURROUNDING
-	override fun getBlockCommentSuffix() = JULIA_DOC_SURROUNDING
-	override fun getLineCommentPrefix() = "# "
-}
