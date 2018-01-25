c4c233e7a4151f74485e62959fbcabe7561a56c4
ice1000 <ice1000kotlin@foxmail.com>
2018-01-25 16:04:37 +0800

    [ parser ] Created parser definition

--- grammar/julia-lexer.flex
+++ grammar/julia-lexer.flex
@@ -7,7 +7,24 @@ import org.ice1000.julia.lang.psi.JuliaTypes;
 
 %%
 
-STR=([^\"\x00-\x1F\x7F\]\|\[\'\"bnrt]|\\u[a-fA-F0-9]{4})*
+%{
+  public JuliaLexer() { this((java.io.Reader) null); }
+%}
+
+%class JuliaLexer
+%implements FlexLexer
+%unicode
+%function advance
+%type IElementType
+%eof{ return;
+%eof}
+
+EOL=\n
+
+INCOMPLETE_STRING=\"([^\"\x00-\x1F\x7F\]\|\[\'\"bnrt]|\\u[a-fA-F0-9]{4})*
+STRING={INCOMPLETE_STRING}\"
 
 %%
 
+{INCOMPLETE_STRING} { return TokenType.BAD_CHARACTER; }
+{STRING} { yybegin(YYINITIAL); return JuliaTypes.STR; }
--- src/org/ice1000/julia/lang/julia-parser.kt
+++ src/org/ice1000/julia/lang/julia-parser.kt
@@ -1,8 +1,28 @@
 package org.ice1000.julia.lang
 
-import com.intellij.psi.tree.IElementType
-import com.intellij.psi.tree.TokenSet
+import com.intellij.lang.*
+import com.intellij.lexer.Lexer
+import com.intellij.openapi.project.Project
+import com.intellij.psi.FileViewProvider
+import com.intellij.psi.PsiElement
+import com.intellij.psi.tree.*
 import org.ice1000.julia.lang.psi.JuliaTypes
+
+class JuliaParserDefinition : ParserDefinition {
+	private companion object {
+		private val FILE = IFileElementType(Language.findInstance(JuliaLanguage::class.java))
+	}
+
+	override fun createParser(project: Project?) = JuliaParser()
+	override fun createFile(viewProvider: FileViewProvider) = JuliaFile(viewProvider)
+	override fun spaceExistanceTypeBetweenTokens(left: ASTNode?, right: ASTNode?) = ParserDefinition.SpaceRequirements.MAY
+	override fun getStringLiteralElements() = JuliaTokenType.STRINGS
+	override fun getFileNodeType() = FILE
+	override fun createLexer(project: Project?) = JuliaLexerAdapter()
+	override fun createElement(node: ASTNode?): PsiElement = JuliaTypes.Factory.createElement(node)
+	override fun getCommentTokens() = JuliaTokenType.COMMENTS
+	override fun getWhitespaceTokens(): TokenSet = TokenSet.WHITE_SPACE
+}
 
 class JuliaTokenType(debugName: String) : IElementType(debugName, JuliaLanguage) {
 	companion object {
