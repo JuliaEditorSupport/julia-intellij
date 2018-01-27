ab89344e84cae5fd5341ca9dd01a2cfdb4127581
ice1000 <ice1000kotlin@foxmail.com>
2018-01-28 01:24:59 +0800

    [ code style ] Fuck @zxj5470

--- test/org/ice1000/julia/lang/julia-char-escape-hint.kt
+++ test/org/ice1000/julia/lang/julia-char-escape-hint.kt
@@ -9,52 +9,52 @@ import java.util.*
  */
 class JuliaCharEscapeHintTest {
 
-    @Test
-    fun testCharEscape() {
-        testNull()
-        testSucceed()
-    }
+	@Test
+	fun testCharEscape() {
+		testNull()
+		testSucceed()
+	}
 
-    @Test
-    fun testNull() {
-        unicodeUToChar("") shouldBe null
-    }
+	@Test
+	fun testNull() {
+		unicodeUToChar("") shouldBe null
+	}
 
-    @Test
-    fun testSucceed() {
-        val iceU = """\u51b0"""
-        val iceX = """\xe5\x86\xb0"""
-        val ice = '冰'
-        unicodeUToChar(iceU) shouldBe ice
-        unicodeXToChar(iceX) shouldBe ice
-    }
+	@Test
+	fun testSucceed() {
+		val iceU = """\u51b0"""
+		val iceX = """\xe5\x86\xb0"""
+		val ice = '冰'
+		unicodeUToChar(iceU) shouldBe ice
+		unicodeXToChar(iceX) shouldBe ice
+	}
 }
 
 fun unicodeUToChar(sourceStr: String): Char? {
-    if (sourceStr.length != 6) return null
-    return Integer.parseInt(sourceStr.substring(2), 16).toChar() // 16进制parse整形字符串。
+	if (sourceStr.length != 6) return null
+	return Integer.parseInt(sourceStr.substring(2), 16).toChar() // 16进制parse整形字符串。
 }
 
 //fun simpleEscapeChar(sourceStr: String): Char = sourceStr[1]
 
 // TODO: effect needs to be enhanced
 fun unicodeXToChar(sourceStr: String): Char? {
-    println(sourceStr.length)
-    val bytes = sourceStr.splitWithLength(4).mapTo(ArrayList()) { it.removePrefix("\\x").parseToInt().toByte() }.toByteArray()
-    return String(bytes, StandardCharsets.UTF_8)[0]
+	println(sourceStr.length)
+	val bytes = sourceStr.splitWithLength(4).mapTo(ArrayList()) { it.removePrefix("\\x").parseToInt().toByte() }.toByteArray()
+	return String(bytes, StandardCharsets.UTF_8)[0]
 }
 
 // TODO: effect needs to be enhanced
 fun String.splitWithLength(len: Int): Array<String> {
-    val ret = mutableListOf<String>()
-    var i = 0
-    while (i + len < length) {
-        ret.add(substring(i, i + len))
-        i += len
-    }
-    ret.add(substring(i))
-    return ret.toTypedArray()
+	val ret = mutableListOf<String>()
+	var i = 0
+	while (i + len < length) {
+		ret.add(substring(i, i + len))
+		i += len
+	}
+	ret.add(substring(i))
+	return ret.toTypedArray()
 }
 
 fun String.parseToInt() = Integer.parseInt(this, 16)
-infix fun Any?.shouldBe(other:Any?)=assert(this==other)
+infix fun Any?.shouldBe(other: Any?) = assert(this == other)
