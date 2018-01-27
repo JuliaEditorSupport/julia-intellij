861ffb612d39a86a623821f5a6384256fb1f4090
ice1000 <ice1000kotlin@foxmail.com>
2018-01-28 03:14:42 +0800

    [ annotator ] Convert to methods

--- src/org/ice1000/julia/lang/editing/julia-annotator.kt
+++ src/org/ice1000/julia/lang/editing/julia-annotator.kt
@@ -18,111 +18,127 @@ class JuliaAnnotator : Annotator {
 					.textAttributes = JuliaHighlighter.ABSTRACT_TYPE_NAME
 			is JuliaModuleName -> holder.createInfoAnnotation(element, null)
 					.textAttributes = JuliaHighlighter.MODULE_NAME
-			is JuliaCharLit -> when (element.textLength) {
-			// 0, 1, 2 are impossible, 3: 'a'
-				0, 1, 2, 3 -> {
-				}
-			// '\n'
-				4 -> if (element.text[2] !in "ux") holder.createInfoAnnotation(element.textRange.narrow(1, 1), null)
-						.textAttributes = JuliaHighlighter.CHAR_ESCAPE
-				else holder.createErrorAnnotation(element.textRange.narrow(1, 1),
-						JuliaBundle.message("julia.lint.invalid-char-escape"))
-						.textAttributes = JuliaHighlighter.CHAR_ESCAPE_INVALID
-			// '\x00'
-				6 -> {
-					if (element.text.trimQuotePair().matches(Regex(JULIA_CHAR_SINGLE_UNICODE_X_REGEX)))
-						holder.createInfoAnnotation(element.textRange.narrow(1, 1), null).textAttributes = JuliaHighlighter.CHAR_ESCAPE
-					else holder.createErrorAnnotation(element.textRange.narrow(1, 1), JuliaBundle.message("julia.lint.invalid-char-escape"))
-							.textAttributes = JuliaHighlighter.CHAR_ESCAPE_INVALID
-				}
-			// '\u0022'
-				8 -> {
-					if (element.text.trimQuotePair().matches(Regex(JULIA_CHAR_SINGLE_UNICODE_U_REGEX)))
-						holder.createInfoAnnotation(element.textRange.narrow(1, 1), null).textAttributes = JuliaHighlighter.CHAR_ESCAPE
-					else holder.createErrorAnnotation(element.textRange.narrow(1, 1), JuliaBundle.message("julia.lint.invalid-char-escape"))
-							.textAttributes = JuliaHighlighter.CHAR_ESCAPE_INVALID
-				}
-			// '\xe5\x86\xb0'
-				14 -> {
-					if (element.text.trimQuotePair().matches(Regex(JULIA_CHAR_TRIPLE_UNICODE_X_REGEX)))
-						holder.createInfoAnnotation(element.textRange.narrow(1, 1), null).textAttributes = JuliaHighlighter.CHAR_ESCAPE
-					else holder.createErrorAnnotation(element.textRange.narrow(1, 1), JuliaBundle.message("julia.lint.invalid-char-escape"))
-							.textAttributes = JuliaHighlighter.CHAR_ESCAPE_INVALID
-				}
-				else -> holder.createErrorAnnotation(element.textRange.narrow(1, 1),
-						JuliaBundle.message("julia.lint.invalid-char-escape"))
-						.textAttributes = JuliaHighlighter.CHAR_ESCAPE_INVALID
-			}
-			is JuliaInteger -> holder.createInfoAnnotation(element, JuliaBundle.message("julia.lint.int")).run {
-				val code = element.text
-				when {
-					code.startsWith("0x") -> {
-						val value = code.substring(2).toInt(16)
-						registerFix(JuliaReplaceWithTextIntention(element, value.toString(),
-								JuliaBundle.message("julia.lint.int-replace-dec")))
-						registerFix(JuliaReplaceWithTextIntention(element, "0b${value.toString(2)}",
-								JuliaBundle.message("julia.lint.int-replace-bin")))
-						registerFix(JuliaReplaceWithTextIntention(element, "0o${value.toString(8)}",
-								JuliaBundle.message("julia.lint.int-replace-oct")))
-					}
-					code.startsWith("0b") -> {
-						val value = code.substring(2).toInt(2)
-						registerFix(JuliaReplaceWithTextIntention(element, value.toString(),
-								JuliaBundle.message("julia.lint.int-replace-dec")))
-						registerFix(JuliaReplaceWithTextIntention(element, "0x${value.toString(16)}",
-								JuliaBundle.message("julia.lint.int-replace-hex")))
-						registerFix(JuliaReplaceWithTextIntention(element, "0o${value.toString(8)}",
-								JuliaBundle.message("julia.lint.int-replace-oct")))
-					}
-					code.startsWith("0o") -> {
-						val value = code.substring(2).toInt(8)
-						registerFix(JuliaReplaceWithTextIntention(element, value.toString(),
-								JuliaBundle.message("julia.lint.int-replace-dec")))
-						registerFix(JuliaReplaceWithTextIntention(element, "0b${value.toString(2)}",
-								JuliaBundle.message("julia.lint.int-replace-bin")))
-						registerFix(JuliaReplaceWithTextIntention(element, "0x${value.toString(16)}",
-								JuliaBundle.message("julia.lint.int-replace-hex")))
-					}
-					else -> {
-						val value = code.toInt()
-						registerFix(JuliaReplaceWithTextIntention(element, "0b${value.toString(2)}",
-								JuliaBundle.message("julia.lint.int-replace-bin")))
-						registerFix(JuliaReplaceWithTextIntention(element, "0o${value.toString(8)}",
-								JuliaBundle.message("julia.lint.int-replace-oct")))
-						registerFix(JuliaReplaceWithTextIntention(element, "0x${value.toString(16)}",
-								JuliaBundle.message("julia.lint.int-replace-hex")))
-					}
-				}
-			}
+			is JuliaCharLit -> char(element, holder)
+			is JuliaInteger -> integer(element, holder)
+			is JuliaString -> string(element, holder)
 			is JuliaFloatLit -> holder.createInfoAnnotation(element, null).run {
 			}
-			is JuliaString -> {
-				val str = element.text.trimQuotePair()
-				fun markEscapeChars(escapeString: String, expandSize: Int, matchRegex: String) {
-					str.indicesOf(escapeString).forEach {
-						if (it + expandSize < str.length) {
-							val s = str.subSequence(it, it + expandSize)
-							if (s.matches(Regex(matchRegex))) {
-								holder.createInfoAnnotation(
-										element.textRange.subRangeBeginOffsetAndLength(it + 1, expandSize),
-										null)
-										.textAttributes = JuliaHighlighter.STRING_ESCAPE
-							} else {
-								holder.createErrorAnnotation(
-										element.textRange.subRangeBeginOffsetAndLength(it + 1, expandSize),
-										JuliaBundle.message("julia.lint.invalid-char-escape"))
-										.textAttributes = JuliaHighlighter.STRING_ESCAPE_INVALID
-							}
-						} else {
-							holder.createErrorAnnotation(
-									element.textRange.narrow(it + 1, 1),//to the end
-									JuliaBundle.message("julia.lint.invalid-char-escape"))
-									.textAttributes = JuliaHighlighter.STRING_ESCAPE_INVALID
-						}
+		}
+	}
+
+	private fun char(
+			element: JuliaCharLit,
+			holder: AnnotationHolder) {
+		when (element.textLength) {
+		// 0, 1, 2 are impossible, 3: 'a'
+			0, 1, 2, 3 -> {
+			}
+		// '\n'
+			4 -> if (element.text[2] !in "ux") holder.createInfoAnnotation(element.textRange.narrow(1, 1), null)
+				.textAttributes = JuliaHighlighter.CHAR_ESCAPE
+			else holder.createErrorAnnotation(element.textRange.narrow(1, 1),
+				JuliaBundle.message("julia.lint.invalid-char-escape"))
+				.textAttributes = JuliaHighlighter.CHAR_ESCAPE_INVALID
+		// '\x00'
+			6 -> {
+				if (element.text.trimQuotePair().matches(Regex(JULIA_CHAR_SINGLE_UNICODE_X_REGEX)))
+					holder.createInfoAnnotation(element.textRange.narrow(1, 1), null).textAttributes = JuliaHighlighter.CHAR_ESCAPE
+				else holder.createErrorAnnotation(element.textRange.narrow(1, 1), JuliaBundle.message("julia.lint.invalid-char-escape"))
+					.textAttributes = JuliaHighlighter.CHAR_ESCAPE_INVALID
+			}
+		// '\u0022'
+			8 -> {
+				if (element.text.trimQuotePair().matches(Regex(JULIA_CHAR_SINGLE_UNICODE_U_REGEX)))
+					holder.createInfoAnnotation(element.textRange.narrow(1, 1), null).textAttributes = JuliaHighlighter.CHAR_ESCAPE
+				else holder.createErrorAnnotation(element.textRange.narrow(1, 1), JuliaBundle.message("julia.lint.invalid-char-escape"))
+					.textAttributes = JuliaHighlighter.CHAR_ESCAPE_INVALID
+			}
+		// '\xe5\x86\xb0'
+			14 -> {
+				if (element.text.trimQuotePair().matches(Regex(JULIA_CHAR_TRIPLE_UNICODE_X_REGEX)))
+					holder.createInfoAnnotation(element.textRange.narrow(1, 1), null).textAttributes = JuliaHighlighter.CHAR_ESCAPE
+				else holder.createErrorAnnotation(element.textRange.narrow(1, 1), JuliaBundle.message("julia.lint.invalid-char-escape"))
+					.textAttributes = JuliaHighlighter.CHAR_ESCAPE_INVALID
+			}
+			else -> holder.createErrorAnnotation(element.textRange.narrow(1, 1),
+				JuliaBundle.message("julia.lint.invalid-char-escape"))
+				.textAttributes = JuliaHighlighter.CHAR_ESCAPE_INVALID
+		}
+	}
+
+	private fun string(
+			element: JuliaString,
+			holder: AnnotationHolder) {
+		val str = element.text.trimQuotePair()
+		fun markEscapeChars(escapeString: String, expandSize: Int, matchRegex: String) {
+			str.indicesOf(escapeString).forEach {
+				if (it + expandSize < str.length) {
+					val s = str.subSequence(it, it + expandSize)
+					if (s.matches(Regex(matchRegex))) {
+						holder.createInfoAnnotation(
+							element.textRange.subRangeBeginOffsetAndLength(it + 1, expandSize),
+							null)
+							.textAttributes = JuliaHighlighter.STRING_ESCAPE
+					} else {
+						holder.createErrorAnnotation(
+							element.textRange.subRangeBeginOffsetAndLength(it + 1, expandSize),
+							JuliaBundle.message("julia.lint.invalid-char-escape"))
+							.textAttributes = JuliaHighlighter.STRING_ESCAPE_INVALID
 					}
+				} else {
+					holder.createErrorAnnotation(
+						element.textRange.narrow(it + 1, 1),//to the end
+						JuliaBundle.message("julia.lint.invalid-char-escape"))
+						.textAttributes = JuliaHighlighter.STRING_ESCAPE_INVALID
 				}
-				markEscapeChars("\\x", 4, JULIA_CHAR_SINGLE_UNICODE_X_REGEX)
-				markEscapeChars("\\u", 6, JULIA_CHAR_SINGLE_UNICODE_U_REGEX)
+			}
+		}
+		markEscapeChars("\\x", 4, JULIA_CHAR_SINGLE_UNICODE_X_REGEX)
+		markEscapeChars("\\u", 6, JULIA_CHAR_SINGLE_UNICODE_U_REGEX)
+	}
+
+	private fun integer(
+			element: JuliaInteger,
+			holder: AnnotationHolder) {
+		holder.createInfoAnnotation(element, JuliaBundle.message("julia.lint.int")).run {
+			val code = element.text
+			when {
+				code.startsWith("0x") -> {
+					val value = code.substring(2).toInt(16)
+					registerFix(JuliaReplaceWithTextIntention(element, value.toString(),
+						JuliaBundle.message("julia.lint.int-replace-dec")))
+					registerFix(JuliaReplaceWithTextIntention(element, "0b${value.toString(2)}",
+						JuliaBundle.message("julia.lint.int-replace-bin")))
+					registerFix(JuliaReplaceWithTextIntention(element, "0o${value.toString(8)}",
+						JuliaBundle.message("julia.lint.int-replace-oct")))
+				}
+				code.startsWith("0b") -> {
+					val value = code.substring(2).toInt(2)
+					registerFix(JuliaReplaceWithTextIntention(element, value.toString(),
+						JuliaBundle.message("julia.lint.int-replace-dec")))
+					registerFix(JuliaReplaceWithTextIntention(element, "0x${value.toString(16)}",
+						JuliaBundle.message("julia.lint.int-replace-hex")))
+					registerFix(JuliaReplaceWithTextIntention(element, "0o${value.toString(8)}",
+						JuliaBundle.message("julia.lint.int-replace-oct")))
+				}
+				code.startsWith("0o") -> {
+					val value = code.substring(2).toInt(8)
+					registerFix(JuliaReplaceWithTextIntention(element, value.toString(),
+						JuliaBundle.message("julia.lint.int-replace-dec")))
+					registerFix(JuliaReplaceWithTextIntention(element, "0b${value.toString(2)}",
+						JuliaBundle.message("julia.lint.int-replace-bin")))
+					registerFix(JuliaReplaceWithTextIntention(element, "0x${value.toString(16)}",
+						JuliaBundle.message("julia.lint.int-replace-hex")))
+				}
+				else -> {
+					val value = code.toInt()
+					registerFix(JuliaReplaceWithTextIntention(element, "0b${value.toString(2)}",
+						JuliaBundle.message("julia.lint.int-replace-bin")))
+					registerFix(JuliaReplaceWithTextIntention(element, "0o${value.toString(8)}",
+						JuliaBundle.message("julia.lint.int-replace-oct")))
+					registerFix(JuliaReplaceWithTextIntention(element, "0x${value.toString(16)}",
+						JuliaBundle.message("julia.lint.int-replace-hex")))
+				}
 			}
 		}
 	}
