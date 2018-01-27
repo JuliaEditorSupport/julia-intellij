5be4a665737a2477303c236d11fdceaa9bca040e
ice1000 <ice1000kotlin@foxmail.com>
2018-01-27 14:46:23 +0800

    [ test ] Fix

--- src/org/ice1000/julia/lang/JuliaLanguage.java
+++ src/org/ice1000/julia/lang/JuliaLanguage.java
@@ -1,34 +1,31 @@
-/*
+package org.ice1000.julia.lang;
+
+import com.intellij.lang.Language;
+
+import static org.ice1000.julia.lang.Julia_constantsKt.JULIA_EXTENSION;
+
+/**
  * WARNINGS:
  * - This file MUST NOT be converted to Kotlin!
  * - DO NOT replace <code>String NAME = "Julia"</code> with
- *      <code>String NAME = JuliaBundle.message("julia.name")</code>
- *      but static import JULIA_LANGUAGE_NAME.
- *
+ * <code>String NAME = JuliaBundle.message("julia.name")</code>
+ * but static import JULIA_LANGUAGE_NAME.
+ * <p>
  * ERRORS:
  * - Tests will be failed.
  * - LanguageType `language="Julia"` in plugin.xml will become red.
  *
- * @author: zxj5470
- * @date: 2018-01-27
+ * @author zxj5470
  */
-package org.ice1000.julia.lang;
-
-import com.intellij.lang.Language;
-import org.jetbrains.annotations.NotNull;
-
 public class JuliaLanguage extends Language {
 	public static final JuliaLanguage INSTANCE = new JuliaLanguage();
+	private static final String NAME = "Julia"; // JuliaBundle.message("julia.name")
 
 	private JuliaLanguage() {
-		super(JuliaBundle.message("julia.name"));
-	}
-
-	public @NotNull String getDisplayName() {
-		return JuliaBundle.message("julia.name");
+		super(NAME, "text/" + JULIA_EXTENSION);
 	}
 
 	public boolean isCaseSensitive() {
-		return true;
+		return false;
 	}
 }
