e8c3ad0800087d80913679cd4eb4f7ee7beb5453
ice1000 <ice1000kotlin@foxmail.com>
2018-01-31 15:08:20 +0800

    [ #39 ] Refactored to injected methods

--- src/org/ice1000/julia/lang/psi/impl/julia-psi-impl.kt
+++ src/org/ice1000/julia/lang/psi/impl/julia-psi-impl.kt
@@ -4,7 +4,10 @@ package org.ice1000.julia.lang.psi.impl
 
 import com.intellij.psi.PsiElement
 import com.intellij.psi.ResolveState
+import com.intellij.psi.impl.source.tree.injected.StringLiteralEscaper
 import com.intellij.psi.scope.PsiScopeProcessor
+import org.ice1000.julia.lang.JuliaTokenType
+import org.ice1000.julia.lang.psi.JuliaStringContent
 
 fun PsiElement.processDeclTrivial(
 	processor: PsiScopeProcessor,
@@ -19,3 +22,6 @@ fun PsiElement.processDeclTrivial(
 	return true
 }
 
+fun JuliaStringContent.isValidHost() = true
+fun JuliaStringContent.createLiteralTextEscaper() = StringLiteralEscaper(this)
+fun JuliaStringContent.updateText(s: String) = replace(JuliaTokenType.fromText(s, project)) as JuliaStringContent
--- src/org/ice1000/julia/lang/psi/impl/julia-psi-mixin.kt
+++ src/org/ice1000/julia/lang/psi/impl/julia-psi-mixin.kt
@@ -1,26 +1,15 @@
 package org.ice1000.julia.lang.psi.impl
 
-import com.intellij.extapi.psi.ASTWrapperPsiElement
-import com.intellij.lang.ASTNode
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.AbstractElementManipulator
-import com.intellij.psi.impl.source.tree.injected.StringLiteralEscaper
-import org.ice1000.julia.lang.JuliaTokenType
 import org.ice1000.julia.lang.psi.JuliaStringContent
 
-
-abstract class JuliaStringContentMixin(astNode: ASTNode) : ASTWrapperPsiElement(astNode), JuliaStringContent {
-	override fun isValidHost() = true
-	override fun createLiteralTextEscaper() = StringLiteralEscaper(this)
-	override fun updateText(s: String) = replace(JuliaTokenType.fromText(s, project)) as JuliaStringContentMixin
-}
-
-class JuliaStringManipulator : AbstractElementManipulator<JuliaStringContentMixin>() {
-	override fun getRangeInElement(element: JuliaStringContentMixin) = TextRange(0, element.textLength)
+class JuliaStringManipulator : AbstractElementManipulator<JuliaStringContent>() {
+	override fun getRangeInElement(element: JuliaStringContent) = TextRange(0, element.textLength)
 	override fun handleContentChange(
-		psi: JuliaStringContentMixin,
+		psi: JuliaStringContent,
 		range: TextRange,
-		new: String): JuliaStringContentMixin {
+		new: String): JuliaStringContent {
 		val oldText = psi.text
 		return psi.updateText("${oldText.substring(0, range.startOffset)}$new${oldText.substring(range.endOffset)}")
 	}
