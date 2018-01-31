108368a9103754b9f144ad8eeb379c4cb0d16e66
ice1000 <ice1000kotlin@foxmail.com>
2018-01-31 15:21:55 +0800

    [ #39 ] Re-refactored

--- src/org/ice1000/julia/lang/psi/impl/julia-psi-mixin.kt
+++ src/org/ice1000/julia/lang/psi/impl/julia-psi-mixin.kt
@@ -1,15 +1,25 @@
 package org.ice1000.julia.lang.psi.impl
 
+import com.intellij.extapi.psi.ASTWrapperPsiElement
+import com.intellij.lang.ASTNode
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.AbstractElementManipulator
+import com.intellij.psi.impl.source.tree.injected.StringLiteralEscaper
+import org.ice1000.julia.lang.JuliaTokenType
 import org.ice1000.julia.lang.psi.JuliaStringContent
 
-class JuliaStringManipulator : AbstractElementManipulator<JuliaStringContent>() {
-	override fun getRangeInElement(element: JuliaStringContent) = TextRange(0, element.textLength)
+abstract class JuliaStringContentMixin(astNode: ASTNode) : ASTWrapperPsiElement(astNode), JuliaStringContent {
+	override fun isValidHost() = true
+	override fun createLiteralTextEscaper() = StringLiteralEscaper(this)
+	override fun updateText(s: String) = replace(JuliaTokenType.fromText(s, project)) as JuliaStringContentMixin
+}
+
+class JuliaStringManipulator : AbstractElementManipulator<JuliaStringContentMixin>() {
+	override fun getRangeInElement(element: JuliaStringContentMixin) = TextRange(0, element.textLength)
 	override fun handleContentChange(
-		psi: JuliaStringContent,
+		psi: JuliaStringContentMixin,
 		range: TextRange,
-		new: String): JuliaStringContent {
+		new: String): JuliaStringContentMixin {
 		val oldText = psi.text
 		return psi.updateText("${oldText.substring(0, range.startOffset)}$new${oldText.substring(range.endOffset)}")
 	}
