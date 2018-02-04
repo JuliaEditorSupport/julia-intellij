783fc04be9cf4bb6a36482a67db5be8a160394d0
ice1000 <ice1000kotlin@foxmail.com>
2018-02-05 02:45:57 +0800

    [ parser ] Bug fixes

--- src/org/ice1000/julia/lang/julia-highlighter.kt
+++ src/org/ice1000/julia/lang/julia-highlighter.kt
@@ -77,6 +77,7 @@ object JuliaHighlighter : SyntaxHighlighter {
 		JuliaTypes.CONTINUE_KEYWORD,
 		JuliaTypes.EXPORT_KEYWORD,
 		JuliaTypes.IMPORT_KEYWORD,
+		JuliaTypes.IMPORTALL_KEYWORD,
 		JuliaTypes.USING_KEYWORD,
 		JuliaTypes.IF_KEYWORD,
 		JuliaTypes.ELSEIF_KEYWORD,
