64d9fe09262437c4edda24e386aadaeb25b9fb84
ice1000 <ice1000kotlin@foxmail.com>
2018-02-05 01:02:45 +0800

    [ bugs ] Fix some bugs

--- src/org/ice1000/julia/lang/julia-highlighter.kt
+++ src/org/ice1000/julia/lang/julia-highlighter.kt
@@ -60,7 +60,7 @@ object JuliaHighlighter : SyntaxHighlighter {
 
 	private val UNICODE_OPERATOR_LIST = listOf(
 		JuliaTypes.MISC_ARROW_SYM,
-		JuliaTypes.MISC_COMPARISON_OP,
+		JuliaTypes.MISC_COMPARISON_SYM,
 		JuliaTypes.MISC_PLUS_SYM,
 		JuliaTypes.MISC_MULTIPLY_SYM,
 		JuliaTypes.MISC_EXPONENT_SYM
