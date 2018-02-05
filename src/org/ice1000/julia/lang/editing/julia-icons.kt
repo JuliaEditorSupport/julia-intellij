d1ada54b0b9a5e1f2e7337de4e6c56e611278179
ice1000 <ice1000kotlin@foxmail.com>
2018-02-05 22:33:35 +0800

    [ icon ] @LimbolRain look at this code

--- src/org/ice1000/julia/lang/editing/julia-icons.kt
+++ src/org/ice1000/julia/lang/editing/julia-icons.kt
@@ -3,16 +3,15 @@ package org.ice1000.julia.lang.editing
 import com.intellij.ide.IconProvider
 import com.intellij.openapi.util.IconLoader
 import com.intellij.psi.PsiElement
-import com.intellij.util.PlatformIcons
 import org.ice1000.julia.lang.JuliaFile
-import org.ice1000.julia.lang.psi.JuliaModuleDeclaration
-import org.ice1000.julia.lang.psi.JuliaStatements
+import org.ice1000.julia.lang.psi.*
 import javax.swing.Icon
 
 @JvmField val JULIA_BIG_ICON = IconLoader.getIcon("/icons/julia.png")
 @JvmField val JULIA_ICON = IconLoader.getIcon("/icons/julia_file.png")
 @JvmField val JOJO_ICON = IconLoader.getIcon("/icons/jojo.png")
 @JvmField val JULIA_MODULE_ICON: Icon = IconLoader.getIcon("/icons/module.png")
+@JvmField val JULIA_TYPE_ICON: Icon = IconLoader.getIcon("/icons/type.png")
 
 class JuliaIconProvider : IconProvider() {
 	override fun getIcon(element: PsiElement, flags: Int): Icon? {
@@ -20,7 +19,11 @@ class JuliaIconProvider : IconProvider() {
 		val statements = file.children
 			.firstOrNull { it is JuliaStatements }
 			?.let { it as? JuliaStatements } ?: return JULIA_ICON
-		if (statements.children.size != 1 || statements.firstChild !is JuliaModuleDeclaration) return JULIA_ICON
-		return JULIA_MODULE_ICON
+		if (statements.children.size != 1) return JULIA_ICON
+		return when (statements.firstChild) {
+			is JuliaModuleDeclaration -> JULIA_MODULE_ICON
+			is JuliaType -> JULIA_TYPE_ICON
+			else -> JULIA_ICON
+		}
 	}
 }
