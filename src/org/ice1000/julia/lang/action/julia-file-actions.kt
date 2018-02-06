7b23b10302fc66a26aba2d154117db4b45488cb7
ice1000 <ice1000kotlin@foxmail.com>
2018-02-06 22:27:45 +0800

    [ #66 ] It works, start refactoring baka's codes

--- src/org/ice1000/julia/lang/action/julia-file-actions.kt
+++ src/org/ice1000/julia/lang/action/julia-file-actions.kt
@@ -1,8 +1,6 @@
 package org.ice1000.julia.lang.action
 
-import com.intellij.ide.actions.CreateFileAction
-import com.intellij.ide.actions.CreateFileFromTemplateAction
-import com.intellij.ide.actions.CreateFileFromTemplateDialog
+import com.intellij.ide.actions.*
 import com.intellij.ide.fileTemplates.FileTemplate
 import com.intellij.ide.fileTemplates.FileTemplateManager
 import com.intellij.ide.fileTemplates.actions.AttributesDefaults
@@ -10,21 +8,18 @@ import com.intellij.ide.fileTemplates.ui.CreateFromTemplateDialog
 import com.intellij.ide.util.projectWizard.AbstractNewProjectStep
 import com.intellij.ide.util.projectWizard.ProjectSettingsStepBase
 import com.intellij.openapi.application.runWriteAction
-import com.intellij.openapi.project.DumbAware
-import com.intellij.openapi.project.DumbService
-import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.*
 import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.psi.*
-import com.intellij.util.IncorrectOperationException
 import icons.JuliaIcons
 import org.ice1000.julia.lang.*
 import org.ice1000.julia.lang.module.JuliaProjectGenerator
 import org.ice1000.julia.lang.module.JuliaSettings
 import java.time.LocalDate
-import java.util.Properties
+import java.util.*
 
 
-inline fun createFile( name : String , directory : PsiDirectory , template : () -> String = {""} ) : Array<PsiElement> {
+inline fun createFile(name: String, directory: PsiDirectory, template: () -> String = { "" }): Array<PsiElement> {
 	val fixedExtension = when (FileUtilRt.getExtension(name)) {
 		JULIA_EXTENSION -> name
 		else -> "$name.$JULIA_EXTENSION"
@@ -54,9 +49,9 @@ class NewJuliaFile : CreateFileAction(
 	JuliaBundle.message("julia.actions.new-file.description"),
 	JuliaIcons.JULIA_ICON) {
 
-	override fun getActionName(directory : PsiDirectory?, s : String?) = JuliaBundle.message("julia.actions.new-file.title")
+	override fun getActionName(directory: PsiDirectory?, s: String?) = JuliaBundle.message("julia.actions.new-file.title")
 	override fun getDefaultExtension() = JULIA_EXTENSION
-	override fun create(name : String, directory : PsiDirectory) : Array<PsiElement> =
+	override fun create(name: String, directory: PsiDirectory): Array<PsiElement> =
 		createFile(name, directory)
 }
 
@@ -70,63 +65,45 @@ class NewJuliaFileFromTemplate : CreateFileFromTemplateAction(
 	JuliaBundle.message("julia.actions.new-file-template.description"),
 	JuliaIcons.JULIA_ICON), DumbAware {
 	companion object {
-		private fun findOrCreateTarget(dir: PsiDirectory, name: String, directorySeparators: Array<Char>): Pair<String, PsiDirectory> {
+		private fun findOrCreateTarget(dir: PsiDirectory, name: String, separators: CharArray): Pair<String, PsiDirectory> {
 			var className = name.removeSuffix(".jl")
 			var targetDir = dir
-
-			for (splitChar in directorySeparators) {
-				if (splitChar in className) {
-					val names = className.trim().split(splitChar)
-
-					for (dirName in names.dropLast(1)) {
-						targetDir = targetDir.findSubdirectory(dirName) ?: runWriteAction {
-							targetDir.createSubdirectory(dirName)
-						}
-					}
-
-					className = names.last()
-					break
-				}
+			separators.firstOrNull { it in className }?.let {
+				val names = className.trim().split(it).dropLast(1)
+				for (dirName in names) targetDir = targetDir.findSubdirectory(dirName) ?: runWriteAction { targetDir.createSubdirectory(dirName) }
+				className = names.last()
 			}
-			return Pair(className, targetDir)
+			return className to targetDir
 		}
 
 		private fun createFromTemplate(dir: PsiDirectory, className: String, template: FileTemplate): PsiFile? {
 			val project = dir.project
 			val defaultProperties = FileTemplateManager.getInstance(project).defaultProperties
-
-			val element = try {
-				CreateFromTemplateDialog(project, dir, template,
-					AttributesDefaults(className).withFixedName(true),
-					Properties(defaultProperties)).create()
-			}
-			catch (e: IncorrectOperationException) {
-				throw e
-			}
-			catch (e: Exception) {
-				LOG.error(e)
-				return null
-			}
-
-			return element?.containingFile
+			return CreateFromTemplateDialog(project, dir, template,
+				AttributesDefaults(className).withFixedName(true),
+				Properties(defaultProperties))
+				.create().containingFile
 		}
 
 		fun createFileFromTemplate(name: String, template: FileTemplate, dir: PsiDirectory): PsiFile? {
-			val directorySeparators = if (template.name == "Julia File") arrayOf('/', '\\') else arrayOf('/', '\\', '.')
+			val directorySeparators = if (template.name == "Julia File") charArrayOf('/', '\\') else charArrayOf('/', '\\', '.')
 			val (className, targetDir) = findOrCreateTarget(dir, name, directorySeparators)
 
 			val service = DumbService.getInstance(dir.project)
 			service.isAlternativeResolveEnabled = true
 			try {
 				return createFromTemplate(targetDir, className, template)
-			}
-			finally {
+			} finally {
 				service.isAlternativeResolveEnabled = false
 			}
 		}
 	}
 
-	override fun getActionName(directory: PsiDirectory?, s: String?, s2: String?): String = JuliaBundle.message("julia.actions.new-file.title")
+	override fun getActionName(
+		directory: PsiDirectory?,
+		s: String?,
+		s2: String?): String = JuliaBundle.message("julia.actions.new-file.title")
+
 	override fun buildDialog(project: Project, directory: PsiDirectory, builder: CreateFileFromTemplateDialog.Builder) {
 		builder
 			.addKind("File", JuliaIcons.JULIA_ICON, "Julia File")
