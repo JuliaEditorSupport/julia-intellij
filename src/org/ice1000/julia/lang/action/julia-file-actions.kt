5bee2270a44ec9657e4c14b42ae03917f130cfbb
Hoshino Tented <limbolrain@gmail.com>
2018-02-06 11:37:35 +0800

    [ action ] new two actions for creating files

--- src/org/ice1000/julia/lang/action/julia-file-actions.kt
+++ src/org/ice1000/julia/lang/action/julia-file-actions.kt
@@ -10,29 +10,95 @@ import org.ice1000.julia.lang.*
 import org.ice1000.julia.lang.module.JuliaProjectGenerator
 import org.ice1000.julia.lang.module.JuliaSettings
 import java.time.LocalDate
+import java.util.regex.Pattern
+import kotlin.reflect.KProperty
+
+object ActionProperties {
+	operator fun getValue(_this : Any?, properties : KProperty<*>) : String{
+		val matcher = Pattern.compile("new([A-Z][a-z]+)(Title|Description)").matcher(properties.name)
+
+		return if( matcher.matches() ) {
+			JuliaBundle.message("julia.actions.new-${matcher.group(1).toLowerCase()}.${matcher.group(2).toLowerCase()}")
+		} else ""
+	}
+}
+
+val newFileTitle : String by ActionProperties
+val newFileDescription : String by ActionProperties
+
+val newModuleTitle : String by ActionProperties
+val newModuleDescription : String by ActionProperties
+
+val newTypeTitle : String by ActionProperties
+val newTypeDescription : String by ActionProperties
+
+inline fun createFile( name : String , directory : PsiDirectory , template : () -> String = {""} ) : Array<PsiElement> {
+	val fixedExtension = when (FileUtilRt.getExtension(name)) {
+		JULIA_EXTENSION -> name
+		else -> "$name.$JULIA_EXTENSION"
+	}
+	return arrayOf(
+		directory.add(
+			PsiFileFactory
+				.getInstance(directory.project)
+				.createFileFromText(
+					fixedExtension, JuliaFileType,
+					"""
+						|$JULIA_BLOCK_COMMENT_BEGIN
+						|# ${FileUtilRt.getNameWithoutExtension(name)}
+						|${JuliaBundle.message("julia.actions.new-file.content", System.getProperty("user.name"), LocalDate.now())}
+						|$JULIA_BLOCK_COMMENT_END
+						|
+						|${template()}
+					""".trimMargin()
+				)
+		)
+	)
+}
 
 class NewJuliaFile : CreateFileAction(
-	JuliaBundle.message("julia.actions.new-file.title"),
-	JuliaBundle.message("julia.actions.new-file.description"),
+	newFileTitle,
+	newFileDescription,
 	JuliaIcons.JULIA_ICON) {
-	override fun getActionName(directory: PsiDirectory?, s: String?) =
-		JuliaBundle.message("julia.actions.new-file.title")
 
+	override fun getActionName(directory : PsiDirectory?, s : String?) = newFileTitle
 	override fun getDefaultExtension() = JULIA_EXTENSION
-	override fun create(name: String, directory: PsiDirectory): Array<PsiElement> {
-		val fixedExtension = when (FileUtilRt.getExtension(name)) {
-			JULIA_EXTENSION -> name
-			else -> "$name.$JULIA_EXTENSION"
-		}
-		return arrayOf(directory.add(PsiFileFactory
-			.getInstance(directory.project)
-			.createFileFromText(fixedExtension, JuliaFileType, """$JULIA_BLOCK_COMMENT_BEGIN
-# ${FileUtilRt.getNameWithoutExtension(name)}
-${JuliaBundle.message("julia.actions.new-file.content", System.getProperty("user.name"), LocalDate.now())}
-$JULIA_BLOCK_COMMENT_END
+	override fun create(name : String, directory : PsiDirectory) : Array<PsiElement> =
+		createFile(name, directory)
+}
 
-""")))
-	}
+class NewJuliaModule : CreateFileAction (
+	newModuleTitle,
+	newModuleDescription,
+	JuliaIcons.JULIA_MODULE_ICON
+) {
+	override fun getActionName(directory : PsiDirectory?, newName : String?) : String = newModuleTitle
+	override fun getDefaultExtension() : String? = JULIA_EXTENSION
+	override fun create(newName : String, directory : PsiDirectory) : Array<PsiElement> =
+		createFile(newName, directory) {
+			"""
+				|module ${FileUtilRt.getNameWithoutExtension(newName)}
+				| # TODO
+				|end
+			""".trimMargin()
+		}
+}
+
+class NewJuliaType : CreateFileAction (
+	newTypeTitle,
+	newTypeDescription,
+	JuliaIcons.JULIA_TYPE_ICON
+) {
+	override fun getActionName(directory : PsiDirectory?, newName : String?) : String = newTypeTitle
+	override fun getDefaultExtension() : String? = JULIA_EXTENSION
+	override fun create(newName : String, directory : PsiDirectory) : Array<PsiElement> =
+		createFile(newName, directory) {
+			"""
+				|type ${FileUtilRt.getNameWithoutExtension(newName)}
+				| # TODO
+				|end
+			""".trimMargin()
+		}
 }
 
 /**
