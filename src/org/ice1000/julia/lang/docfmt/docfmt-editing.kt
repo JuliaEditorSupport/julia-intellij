f00f597082b3b1a9784db8b18e1871808a837ab0
LimbolRain <limbolrain@gmail.com>
2018-02-07 13:34:46 +0800

    [ #67 ] :) I love Ubuntu

--- src/org/ice1000/julia/lang/docfmt/docfmt-editing.kt
+++ src/org/ice1000/julia/lang/docfmt/docfmt-editing.kt
@@ -15,30 +15,93 @@ import org.ice1000.julia.lang.editing.JuliaCompletionProvider
 import org.ice1000.julia.lang.editing.JuliaRemoveElementIntention
 
 class DocfmtAnnotator : Annotator {
+	companion object {
+
+		val indentArgs = arrayOf(
+			"CSTParser.Begin", "CSTParser.Quote",
+			"CSTParser.For", "CSTParser.While", "CSTParser.FunctionDef", "CSTParser.Macro",
+			"CSTParser.Struct", "CSTParser.Let", "CSTParser.Try", "CSTParser.If", "CSTParser.Mutable"
+		)
+
+		val noWsOpGroup = arrayOf(
+			"8", "13", "14", "16"
+		)
+	}
+
+	/**
+	 * //IndentWidth : 4
+	 * //KW_WS : true
+	 * //NewLineEOF : false
+	 * //No_WS_OP_group : [8, 13, 14, 16]
+	 * No_WS_OP_indv : Any[]
+	 * //StripLineEnds : false
+	 * //TabWidth : 4
+	 * //UseTab : false
+	 */
+
 	override fun annotate(element: PsiElement, holder: AnnotationHolder) {
 		if (element is DocfmtConfig) {
+			fun unused() {
+				holder.createWeakWarningAnnotation(element, JuliaBundle.message("docfmt.lint.default")).run {
+					highlightType = ProblemHighlightType.LIKE_UNUSED_SYMBOL
+					registerFix(JuliaRemoveElementIntention(element, JuliaBundle.message("docfmt.lint.remove")))
+				}
+			}
+
+			fun invalidValue() = holder.createErrorAnnotation(element, JuliaBundle.message("docfmt.lint.invalid-val", element.text)).run {
+					highlightType = ProblemHighlightType.LIKE_UNKNOWN_SYMBOL
+				}
+
+			fun number() {
+				val value = element.value
+				if (value.firstChild.node.elementType != DocfmtTypes.INT) invalidValue()
+				else if (value.text == "4") unused()
+			}
+
+			fun checkBoolean(default: Boolean = false) {
+			when (element.value.text) {
+				default.toString() -> unused()
+				(!default).toString() -> Unit
+
+				else -> invalidValue()
+			}
+		}
+
 			val key = element.firstChild
 			val value = element.value
 			when (key.text) {
-				"TabWidth" -> number(element, holder)
-				"IndentWidth" -> number(element, holder)
-				"UseTab" -> when (value.text) {
-					"false" -> unused(element, holder)
-					"true" -> Unit
-					else -> invalidValue(value, holder)
-				}
+				"TabWidth" -> number()
+				"IndentWidth" -> number()
+				"UseTab" -> checkBoolean()
 				"AlignAfterOpenBracket" -> when (value.text) {
-					"Align" -> unused(element, holder)
+					"Align" -> unused()
 					"AlwaysBreak", "DontAlign" -> Unit // OK
-					else -> invalidValue(value, holder)
+					else -> invalidValue()
 				}
-				"IndentEXPR" -> Unit // TODO
+				"IndentEXPR" ->
+					if( Companion.indentArgs.contains(value.text) ) Unit
+					else invalidValue()
+				"KW_WS" -> checkBoolean(true)
+				"NewLineEOF" -> checkBoolean()
+				"StripLineEnds" -> checkBoolean()
+				"No_WS_OP_group" ->
+					if( Companion.noWsOpGroup.contains(value.text) ) Unit
+					else invalidValue()
 				else -> holder.createErrorAnnotation(key, JuliaBundle.message("docfmt.lint.invalid", key.text)).run {
 					highlightType = ProblemHighlightType.LIKE_UNKNOWN_SYMBOL
 				}
 			}
 		} else return
 	}
+
+	/* move to annotate function
+	private fun checkBoolean(element: DocfmtConfig, holder: AnnotationHolder, default: Boolean = false) =
+		when( element.value.text ) {
+			default.toString() -> unused(element, holder)
+			(! default).toString() -> Unit
+
+			else -> invalidValue(element, holder)
+		}
 
 	private fun number(element: DocfmtConfig, holder: AnnotationHolder) {
 		val value = element.value
@@ -63,6 +126,8 @@ class DocfmtAnnotator : Annotator {
 		holder.createErrorAnnotation(value, JuliaBundle.message("docfmt.lint.invalid-val", value.text)).run {
 			highlightType = ProblemHighlightType.LIKE_UNKNOWN_SYMBOL
 		}
+
+		*/
 }
 
 class DocfmtCompletionContributor : CompletionContributor() {
