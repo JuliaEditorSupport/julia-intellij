1f61f2b4072abfb023c32a8efa2468e5fc472c8f
ice1000 <ice1000kotlin@foxmail.com>
2018-02-07 14:51:25 +0800

    [ #67 ] Add completions

--- src/org/ice1000/julia/lang/docfmt/docfmt-editing.kt
+++ src/org/ice1000/julia/lang/docfmt/docfmt-editing.kt
@@ -1,29 +1,26 @@
 package org.ice1000.julia.lang.docfmt
 
-import com.intellij.codeInsight.completion.CompletionContributor
-import com.intellij.codeInsight.completion.CompletionType
+import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.patterns.PlatformPatterns.psiElement
 import com.intellij.psi.PsiElement
+import com.intellij.util.ProcessingContext
 import org.ice1000.julia.lang.JuliaBundle
-import org.ice1000.julia.lang.docfmt.psi.DocfmtConfig
-import org.ice1000.julia.lang.docfmt.psi.DocfmtTypes
+import org.ice1000.julia.lang.docfmt.psi.*
 import org.ice1000.julia.lang.editing.JuliaCompletionProvider
 import org.ice1000.julia.lang.editing.JuliaRemoveElementIntention
 
 class DocfmtAnnotator : Annotator {
-	companion object {
-
-		val indentArgs = listOf(
+	companion object  {
+		internal val noWsOpGroup = listOf("8", "13", "14", "16")
+		private val indentArgs = listOf(
 			"CSTParser.Begin", "CSTParser.Quote",
 			"CSTParser.For", "CSTParser.While", "CSTParser.FunctionDef", "CSTParser.Macro",
 			"CSTParser.Struct", "CSTParser.Let", "CSTParser.Try", "CSTParser.If", "CSTParser.Mutable"
 		)
-
-		val noWsOpGroup = listOf("8", "13", "14", "16")
 	}
 
 	/**
@@ -38,58 +35,64 @@ class DocfmtAnnotator : Annotator {
 	 */
 	override fun annotate(element: PsiElement, holder: AnnotationHolder) {
 		if (element is DocfmtConfig) {
-			fun unused() {
-				holder.createWeakWarningAnnotation(element, JuliaBundle.message("docfmt.lint.default")).run {
-					highlightType = ProblemHighlightType.LIKE_UNUSED_SYMBOL
-					registerFix(JuliaRemoveElementIntention(element, JuliaBundle.message("docfmt.lint.remove")))
-				}
+			val key = element.firstChild
+			val value = element.value
+			fun unused() = holder.createWeakWarningAnnotation(element, JuliaBundle.message("docfmt.lint.default")).run {
+				highlightType = ProblemHighlightType.LIKE_UNUSED_SYMBOL
+				registerFix(JuliaRemoveElementIntention(element, JuliaBundle.message("docfmt.lint.remove")))
 			}
 
-			fun invalidValue() = holder.createErrorAnnotation(element, JuliaBundle.message("docfmt.lint.invalid-val", element.text)).run {
+			fun invalidValue(element: PsiElement) = holder.createErrorAnnotation(element, JuliaBundle.message("docfmt.lint.invalid-val", element.text)).run {
 				highlightType = ProblemHighlightType.LIKE_UNKNOWN_SYMBOL
 			}
 
 			fun number() {
-				val value = element.value
-				if (value.firstChild.node.elementType != DocfmtTypes.INT) invalidValue()
+				if (value.firstChild.node.elementType != DocfmtTypes.INT) invalidValue(value)
 				else if (value.text == "4") unused()
 			}
 
-			fun checkBoolean(default: Boolean = false) {
+			fun boolean(default: Boolean = false) {
 				when (element.value.text) {
-					default.toString() -> unused()
-					(!default).toString() -> Unit
-
-					else -> invalidValue()
+					"$default" -> unused()
+					"${!default}" -> Unit
+					else -> invalidValue(value)
 				}
 			}
 
-			val key = element.firstChild
-			val value = element.value
 			when (key.text) {
 				"TabWidth" -> number()
 				"IndentWidth" -> number()
-				"UseTab" -> checkBoolean()
+				"UseTab" -> boolean()
 				"AlignAfterOpenBracket" -> when (value.text) {
 					"Align" -> unused()
 					"AlwaysBreak", "DontAlign" -> Unit // OK
-					else -> invalidValue()
+					else -> invalidValue(value)
 				}
-				"IndentEXPR" -> if (value.text !in indentArgs) invalidValue()
-				"KW_WS" -> checkBoolean(true)
-				"NewLineEOF" -> checkBoolean()
-				"StripLineEnds" -> checkBoolean()
-				"No_WS_OP_group" -> if (value.text !in noWsOpGroup) invalidValue()
-				else -> holder.createErrorAnnotation(key, JuliaBundle.message("docfmt.lint.invalid", key.text)).run {
-					highlightType = ProblemHighlightType.LIKE_UNKNOWN_SYMBOL
-				}
+				"IndentEXPR" -> if (value.text !in indentArgs) invalidValue(value)
+				"KW_WS" -> boolean(true)
+				"NewLineEOF" -> boolean()
+				"StripLineEnds" -> boolean()
+				"No_WS_OP_group" -> if (value.text !in noWsOpGroup) invalidValue(value)
+				"No_WS_OP_indv" -> Unit // TODO
+				else -> invalidValue(key)
 			}
 		} else return
 	}
 }
 
 class DocfmtCompletionContributor : CompletionContributor() {
-	internal companion object CompletionHolder {
+	internal companion object ValueCompleter : CompletionProvider<CompletionParameters>() {
+		override fun addCompletions(p: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
+			val value = p.originalPosition?.parent as? DocfmtValue ?: return
+			val key = (value.parent as? DocfmtConfig)?.firstChild ?: return
+			when (key.text) {
+				"KW_WS", "NewLineEOF", "StripLineEnds", "UseTab" -> BOOLEAN.forEach(result::addElement)
+				"AlignAfterOpenBracket" -> ALIGN.forEach(result::addElement)
+			}
+		}
+
+		private val BOOLEAN = listOf("true", "false").map(LookupElementBuilder::create)
+		private val ALIGN = listOf("Align", "AlwaysBreak", "DontAlign").map(LookupElementBuilder::create)
 		private val VALID_KEY = listOf(
 			"TabWidth",
 			"UseTab",
@@ -103,21 +106,20 @@ class DocfmtCompletionContributor : CompletionContributor() {
 			"NewLineEOF"
 		)
 		private val KEYS = VALID_KEY.map(LookupElementBuilder::create)
-		private val VALUES = listOf(
-			"Align",
-			"true",
-			"false"
-		).map(LookupElementBuilder::create)
+	}
+
+	override fun invokeAutoPopup(position: PsiElement, typeChar: Char): Boolean {
+		return typeChar in "= \n\t" || super.invokeAutoPopup(position, typeChar)
 	}
 
 	init {
 		extend(
 			CompletionType.BASIC,
-			psiElement().afterLeaf(psiElement(DocfmtTypes.EOL)),
+			psiElement().andNot(psiElement().afterLeaf(psiElement(DocfmtTypes.EQ_SYM))),
 			JuliaCompletionProvider(KEYS))
 		extend(
 			CompletionType.BASIC,
 			psiElement().afterLeaf(psiElement(DocfmtTypes.EQ_SYM)),
-			JuliaCompletionProvider(VALUES))
+			ValueCompleter)
 	}
 }
