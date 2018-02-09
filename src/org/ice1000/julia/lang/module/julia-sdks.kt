27dff03bba9ccf15f4825f9346388acab38b328b
ice1000 <ice1000kotlin@foxmail.com>
2018-02-10 00:36:28 +0800

    [ loli ] Why you're adding these

--- src/org/ice1000/julia/lang/module/julia-sdks.kt
+++ src/org/ice1000/julia/lang/module/julia-sdks.kt
@@ -1,19 +1,11 @@
 package org.ice1000.julia.lang.module
 
-import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.projectRoots.*
-import com.intellij.openapi.projectRoots.ui.ProjectJdksEditor
-import com.intellij.openapi.util.SystemInfo
-import com.intellij.ui.ColoredListCellRenderer
-import com.intellij.ui.ComboboxWithBrowseButton
 import icons.JuliaIcons.JULIA_BIG_ICON
 import org.ice1000.julia.lang.*
 import org.jdom.Element
 import java.nio.file.Files
 import java.nio.file.Paths
-import java.util.stream.Collectors
-import javax.swing.DefaultComboBoxModel
-import javax.swing.JList
 
 class JuliaSdkType : SdkType(JuliaBundle.message("julia.name")) {
 	override fun getPresentableName() = JuliaBundle.message("julia.modules.sdk.name")
@@ -21,19 +13,13 @@ class JuliaSdkType : SdkType(JuliaBundle.message("julia.name")) {
 	override fun getIconForAddAction() = icon
 	override fun isValidSdkHome(sdkHome: String?) = validateJuliaSDK(sdkHome.orEmpty())
 	override fun suggestSdkName(s: String?, p1: String?) = JuliaBundle.message("julia.modules.sdk.name")
-	override fun suggestHomePath() = when {
-		SystemInfo.isWindows -> findPathWindows() ?: "C:\\Program Files"
-		SystemInfo.isMac -> findPathMac()
-		else -> findPathLinux() ?: "/usr/share/julia"
-	}
-
+	override fun suggestHomePath() = Paths.get(defaultExePath).parent?.parent?.toString()
 	override fun getDownloadSdkUrl() = JULIA_WEBSITE
-	override fun createAdditionalDataConfigurable(md: SdkModel, m: SdkModificator) = JuliaSdkDataConfigurable()
+	override fun createAdditionalDataConfigurable(md: SdkModel, m: SdkModificator): AdditionalDataConfigurable? = null
 	override fun getVersionString(sdkHome: String?) = versionOf(sdkHome.orEmpty())
 	override fun saveAdditionalData(additionalData: SdkAdditionalData, element: Element) = Unit // leave blank
 	override fun setupSdkPaths(sdk: Sdk, sdkModel: SdkModel): Boolean {
 		val modificator = sdk.sdkModificator
-		modificator.sdkAdditionalData = sdk.sdkAdditionalData ?: JuliaSdkData()
 		modificator.versionString = getVersionString(sdk) ?: JuliaBundle.message("julia.modules.sdk.unknown-version")
 		modificator.commitChanges()
 		return true
@@ -44,64 +30,5 @@ class JuliaSdkType : SdkType(JuliaBundle.message("julia.name")) {
 	}
 }
 
-private fun findPathLinux() = executeCommand("whereis julia", null, 500L)
-	.first
-	.firstOrNull()
-	?.split(' ')
-	?.firstOrNull { Files.isExecutable(Paths.get(it)) }
-	?.let { Paths.get(it).parent.parent.toAbsolutePath().toString() }
-	?: System.getenv("PATH")
-		.split(":")
-		.firstOrNull { Files.isExecutable(Paths.get(it, "julia")) }
-		?.let { Paths.get(it).parent.toAbsolutePath().toString() }
-
-fun SdkAdditionalData?.toJuliaSdkData() = this as? JuliaSdkData
-
-class JuliaSdkData(
-	var tryEvaluateTimeLimit: Long = 2500L,
-	var tryEvaluateTextLimit: Int = 320) : SdkAdditionalData {
-	override fun clone() = JuliaSdkData(tryEvaluateTimeLimit, tryEvaluateTextLimit)
-}
-
 fun validateJuliaSDK(sdkHome: String) = Files.isExecutable(Paths.get(sdkHome, "bin", "julia")) or
 	Files.isExecutable(Paths.get(sdkHome, "bin", "julia.exe"))
-
-val sdkHomePath
-	get() = JuliaSdkType().suggestHomePath()
-
-class JuliaSdkComboBox : ComboboxWithBrowseButton() {
-	val selectedSdk get() = comboBox.selectedItem as? Sdk
-	val sdkName get() = selectedSdk?.name.orEmpty()
-
-	init {
-		comboBox.setRenderer(object : ColoredListCellRenderer<Sdk?>() {
-			override fun customizeCellRenderer(
-				list: JList<out Sdk?>,
-				value: Sdk?,
-				index: Int,
-				selected: Boolean,
-				hasFocus: Boolean) {
-				value?.name?.let(::append)
-			}
-		})
-		addActionListener {
-			var selectedSdk = selectedSdk
-			val project = ProjectManager.getInstance().defaultProject
-			val editor = ProjectJdksEditor(selectedSdk, project, this@JuliaSdkComboBox)
-			editor.title = JuliaBundle.message("julia.modules.sdk.selection.title")
-			editor.show()
-			if (editor.isOK) {
-				selectedSdk = editor.selectedJdk
-				updateSdkList(selectedSdk)
-			}
-		}
-		updateSdkList()
-	}
-
-	private fun updateSdkList(sdkToSelectOuter: Sdk? = null) {
-		ProjectJdkTable.getInstance().getSdksOfType(JuliaSdkType.instance).run {
-			comboBox.model = DefaultComboBoxModel(toTypedArray())
-			comboBox.selectedItem = sdkToSelectOuter ?: firstOrNull()
-		}
-	}
-}
