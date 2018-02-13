59d1f86a6541df1a09cafd89cc81f133b398c8e5
LimbolRain <limbolrain@gmail.com>
2018-02-13 17:27:46 +0800

    [ #79 ] Update RegExp rule

--- src/org/ice1000/julia/lang/editing/julia-regexp.kt
+++ src/org/ice1000/julia/lang/editing/julia-regexp.kt
@@ -6,23 +6,37 @@ import com.intellij.psi.InjectedLanguagePlaces
 import com.intellij.psi.LanguageInjector
 import com.intellij.psi.PsiLanguageInjectionHost
 import org.ice1000.julia.lang.JuliaBundle
+import org.ice1000.julia.lang.psi.JuliaImplicitMultiplyOp
+import org.ice1000.julia.lang.psi.JuliaSymbol
 import org.ice1000.julia.lang.psi.impl.JuliaStringContentImpl
 import org.intellij.lang.regexp.RegExpLanguage
 
 class JuliaRegexpInjector : LanguageInjector {
+
+	/**
+	 * rule: r"RegExp"
+	 */
+	private fun rule(host : JuliaStringContentImpl) : Boolean {
+		val parent = host.parent.parent
+
+		if( parent is JuliaImplicitMultiplyOp ) {
+			val symbol = parent.children.firstOrNull() as? JuliaSymbol
+			//println(symbol?.text)
+			if (symbol != null && symbol.text == "r") {
+				return true
+			}
+		}
+
+		return false
+	}
+
 	override fun getLanguagesToInject(host: PsiLanguageInjectionHost, places: InjectedLanguagePlaces) {
 		if (host is JuliaStringContentImpl) {
-			val prefix = JuliaBundle.message("regexp.pre-fix.text")
-			val text = StringUtil.stripQuotesAroundValue(host.text)
-			if (text.startsWith(prefix)) {
+			if (rule(host)) {
 				places.addPlace(
 					RegExpLanguage.INSTANCE,
-					TextRange(
-						prefix.length,
-						text.length
-					),
-					null,
-					null
+					TextRange(0, host.text.length),
+					null, null
 				)
 			}
 		}
