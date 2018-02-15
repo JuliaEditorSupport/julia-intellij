a701f072b554f4dde20365459924cc8ba328cec0
ice1000 <ice1000kotlin@foxmail.com>
2018-02-16 02:26:56 +0800

    [ doc string ] Improve

--- src/org/ice1000/julia/lang/editing/julia-injectors.kt
+++ src/org/ice1000/julia/lang/editing/julia-injectors.kt
@@ -7,22 +7,30 @@ import org.ice1000.julia.lang.forceRun
 import org.ice1000.julia.lang.psi.*
 import org.intellij.lang.regexp.RegExpLanguage
 
+/**
+ * Invoked before Annotator, so initialize DocStrings here
+ * @author ice1000, HoshinoTented
+ */
 class JuliaLanguageInjector : LanguageInjector {
 	/** regex: r"RegExp" */
-	private fun regex(host: JuliaStringContent): Boolean {
+	private fun regex(host: PsiLanguageInjectionHost): Boolean {
 		val parent = host.parent.parent as? JuliaImplicitMultiplyOp ?: return false
 		val symbol = parent.children.firstOrNull() as? JuliaSymbol
 		return "r" == symbol?.text
 	}
 
 	private fun markdown(host: PsiLanguageInjectionHost) =
-		host.parent.nextSibling?.let { it is JuliaFunction || it.nextSibling is JuliaFunction } == true
+		host.parent.nextSibling?.let { it as? JuliaFunction ?: it.nextSibling as? JuliaFunction }
 
 	override fun getLanguagesToInject(host: PsiLanguageInjectionHost, places: InjectedLanguagePlaces) {
-		host as? JuliaStringContent ?: return
+		if (host !is JuliaStringContent) return
 		when {
-			regex(host) -> places.addPlace(RegExpLanguage.INSTANCE, TextRange(0, host.textLength), null, null)
-			markdown(host) -> forceRun {
+			host.isRegex || regex(host) -> {
+				host.isRegex = true
+				places.addPlace(RegExpLanguage.INSTANCE, TextRange(0, host.textLength), null, null)
+			}
+			host.isDocString || markdown(host)?.let { it.docString = host } != null -> forceRun {
+				host.isDocString = true
 				val markdownLanguage = Language.findLanguageByID("Markdown")
 					?: Language.findLanguageByID("MultiMarkdown")
 					?: return@forceRun
--- src/org/ice1000/julia/lang/psi/impl/julia-psi-mixin.kt
+++ src/org/ice1000/julia/lang/psi/impl/julia-psi-mixin.kt
@@ -34,6 +34,7 @@ interface IJuliaStringContent : PsiLanguageInjectionHost {
 	override fun createLiteralTextEscaper(): StringLiteralEscaper<out JuliaStringContent>
 	override fun updateText(s: String): JuliaStringContent
 	var isDocString: Boolean
+	var isRegex: Boolean
 }
 
 abstract class JuliaStringContentMixin(astNode: ASTNode) : ASTWrapperPsiElement(astNode), JuliaStringContent {
@@ -41,6 +42,12 @@ abstract class JuliaStringContentMixin(astNode: ASTNode) : ASTWrapperPsiElement(
 	override fun createLiteralTextEscaper() = StringLiteralEscaper(this)
 	override fun updateText(s: String) = replace(JuliaTokenType.fromText(s, project)) as JuliaStringContent
 	override var isDocString = false
+	override var isRegex = false
+	override fun subtreeChanged() {
+		isDocString = false
+		isRegex = false
+		super.subtreeChanged()
+	}
 }
 
 interface IJuliaSymbol : JuliaExpr {
