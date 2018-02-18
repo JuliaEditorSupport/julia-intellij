644c678deb81f3c1a4a5d6b8856068773681db1b
ice1000 <ice1000kotlin@foxmail.com>
2018-02-18 23:08:06 +0800

    [ #78 ] Ah oh yeah

--- src/org/ice1000/julia/lang/psi/julia-resolving.kt
+++ src/org/ice1000/julia/lang/psi/julia-resolving.kt
@@ -1,5 +1,6 @@
 package org.ice1000.julia.lang.psi
 
+import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.*
@@ -17,12 +18,20 @@ class JuliaSymbolRef(private val symbol: JuliaSymbol, private var refTo: PsiElem
 	override fun getRangeInElement() = range
 	override fun isSoft() = true
 	override fun resolve() = multiResolve(false).firstOrNull()?.element
-	override fun multiResolve(incompleteCode: Boolean) = ResolveCache.getInstance(symbol.project).resolveWithCaching(this, resolver, true, false)
 	override fun isReferenceTo(element: PsiElement?) = element == refTo || (element as? JuliaSymbol)?.text == symbol.text
-	override fun getVariants(): Array<Any> = emptyArray()
+	override fun getVariants(): Array<LookupElementBuilder> {
+		val variantsProcessor = CompletionProcessor(this, true)
+		treeWalkUp(variantsProcessor, symbol, symbol.containingFile, ResolveState.initial())
+		return variantsProcessor.candidateSet.toTypedArray()
+	}
+
 	override fun getCanonicalText(): String = symbol.text
 	override fun handleElementRename(newName: String) = JuliaTokenType.fromText(newName, symbol.project).let(symbol::replace)
 	override fun bindToElement(element: PsiElement) = symbol.also { refTo = element }
+	override fun multiResolve(incompleteCode: Boolean): Array<out ResolveResult> {
+		if (symbol.project.isDisposed) return emptyArray()
+		return ResolveCache.getInstance(symbol.project).resolveWithCaching(this, resolver, true, false)
+	}
 
 	private companion object ResolverHolder {
 		private val resolver = ResolveCache.PolyVariantResolver<JuliaSymbolRef> { ref, incompleteCode ->
@@ -43,7 +52,7 @@ abstract class ResolveProcessor<ResolveResult>(val place: PsiElement) : PsiScope
 	protected val PsiElement.hasNoError get() = (this as? StubBasedPsiElement<*>)?.stub != null || !PsiTreeUtil.hasErrorElements(this)
 	fun addCandidate(candidate: ResolveResult) = candidateSet.add(candidate)
 
-	protected fun isInScope(element: PsiElement) = true
+	protected fun isInScope(element: PsiElement) = true // TODO
 //		PsiTreeUtil.isAncestor(element.parent?.parent, place, false)
 }
 
@@ -61,5 +70,19 @@ class SymbolResolveProcessor(private val name: String, place: PsiElement, val in
 			!accessible
 		}
 		else -> true
+	}
+}
+
+class CompletionProcessor(place: PsiElement, val incompleteCode: Boolean) :
+	ResolveProcessor<LookupElementBuilder>(place) {
+	constructor(ref: JuliaSymbolRef, incompleteCode: Boolean) : this(ref.element, incompleteCode)
+
+	override fun <T : Any?> getHint(hintKey: Key<T>): T? = null
+	override fun execute(element: PsiElement, resolveState: ResolveState): Boolean {
+		if (element.canResolve and element.hasNoError and isInScope(element)) {
+			val symbol = element.text
+			// TODO add to result
+		}
+		return true
 	}
 }
