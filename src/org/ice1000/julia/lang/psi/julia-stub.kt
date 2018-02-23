1a277d4eab715f40bc6c1de829ddc643130b002a
LimbolRain <limbolrain@gmail.com>
2018-02-24 00:05:50 +0800

    [ #87 #88 ] Add some boring things

--- src/org/ice1000/julia/lang/psi/julia-stub.kt
+++ src/org/ice1000/julia/lang/psi/julia-stub.kt
@@ -4,9 +4,20 @@ import com.intellij.navigation.GotoClassContributor
 import com.intellij.navigation.NavigationItem
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.stubs.StringStubIndexExtension
 import com.intellij.psi.stubs.StubIndex
 import com.intellij.psi.stubs.StubIndexKey
 import org.ice1000.julia.lang.psi.impl.JuliaSymbolMixin
+
+//又抄了点代码过来x
+
+class JuliaNamedElementIndex : StringStubIndexExtension<JuliaSymbolMixin>() {
+	companion object {
+		val KEY : StubIndexKey<String, JuliaSymbolMixin> = StubIndexKey.createIndexKey("org.ice1000.julia.lang.psi.JuliaNamedElementIndex")
+	}
+
+	override fun getKey() = KEY
+}
 
 // TODO this doesn't work at all
 abstract class JuliaNavigationContributorBase<T>
@@ -31,3 +42,8 @@ protected constructor(
 	override fun getQualifiedName(item: NavigationItem?) = (item as? JuliaSymbol)?.name
 	override fun getQualifiedNameSeparator() = "::"
 }
+
+class JuliaSymbolNavigationContributor : JuliaNavigationContributorBase<JuliaSymbolMixin>(
+	JuliaNamedElementIndex.KEY,
+	JuliaSymbolMixin::class.java
+)
