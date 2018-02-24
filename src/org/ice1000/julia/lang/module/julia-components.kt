fd15d819b92331db6a0893431acdc11218c7097a
ice1000 <ice1000kotlin@foxmail.com>
2018-02-25 00:56:00 +0800

    [ #98 ] Why am I so stupid

--- src/org/ice1000/julia/lang/module/julia-components.kt
+++ src/org/ice1000/julia/lang/module/julia-components.kt
@@ -2,8 +2,6 @@ package org.ice1000.julia.lang.module
 
 import com.intellij.ide.plugins.PluginManager
 import com.intellij.notification.*
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.components.AbstractProjectComponent
 import com.intellij.openapi.components.ProjectComponent
 import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.project.Project
@@ -12,31 +10,13 @@ import org.ice1000.julia.lang.JULIA_PLUGIN_ID
 import org.ice1000.julia.lang.JuliaBundle
 import org.jetbrains.annotations.Nls
 
-interface JuliaApplicationComponent : ProjectComponent {
-	companion object InstanceHolder {
-		val instance: JuliaApplicationComponent
-			get() = ApplicationManager.getApplication().getComponent(JuliaApplicationComponent::class.java)
-	}
-
-	val isRelease: Boolean
-	var isNotReleaseNotificationShown: Boolean
-	override fun getComponentName(): String
-	override fun initComponent()
-	override fun projectOpened()
-}
-
-class JuliaApplicationComponentImpl(project: Project) : JuliaApplicationComponent, AbstractProjectComponent(project) {
-	override var isNotReleaseNotificationShown = false
-	override var isRelease = false
+class JuliaApplicationComponent(private val project: Project) : ProjectComponent {
+	var isNotReleaseNotificationShown = false
 
 	override fun getComponentName() = "JuliaApplicationComponent"
-	override fun initComponent() {
-		isRelease = PluginManager.getPlugin(PluginId.getId(JULIA_PLUGIN_ID))?.run { '-' !in version } == true
-	}
-
 	override fun projectOpened() {
-		super.projectOpened()
-		if (!validateJulia(myProject.juliaSettings.settings)) notify(
+		val isRelease = PluginManager.getPlugin(PluginId.getId(JULIA_PLUGIN_ID))?.run { '-' !in version } == true
+		if (!validateJulia(project.juliaSettings.settings)) notify(
 			JuliaBundle.message("julia.messages.notify.invalid-julia.title"),
 			JuliaBundle.message("julia.messages.notify.invalid-julia.content"),
 			NotificationType.WARNING)
