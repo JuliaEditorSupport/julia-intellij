c10f96bc041a85853df1f004b38a3c5a138bd812
ice1000 <ice1000@sbrella.com>
2018-02-26 15:46:44 +0800

    [ #110 ] UI

--- src/org/ice1000/julia/lang/module/ui/JuliaPackageManager.java
+++ src/org/ice1000/julia/lang/module/ui/JuliaPackageManager.java
@@ -1,11 +1,12 @@
 package org.ice1000.julia.lang.module.ui;
 
 import com.intellij.openapi.options.Configurable;
+import com.intellij.ui.ComboboxWithBrowseButton;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
 public abstract class JuliaPackageManager implements Configurable {
 	protected @NotNull JPanel mainPanel;
-	private JComboBox alternativeExecutables;
+	protected @NotNull ComboboxWithBrowseButton alternativeExecutables;
 }
