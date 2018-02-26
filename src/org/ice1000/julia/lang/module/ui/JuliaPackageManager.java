7ba61631f736c5c26b7766d946df88fc5fb8d0f3
zxj5470 <zxj5470@foxmail.com>
2018-02-26 15:32:41 +0800

    [ #110 ] rename executable ui

--- src/org/ice1000/julia/lang/module/ui/JuliaPackageManager.java
+++ src/org/ice1000/julia/lang/module/ui/JuliaPackageManager.java
@@ -7,5 +7,5 @@ import javax.swing.*;
 
 public abstract class JuliaPackageManager implements Configurable {
 	protected @NotNull JPanel mainPanel;
-	private JComboBox comboBox1;
+	private JComboBox alternativeExecutables;
 }
