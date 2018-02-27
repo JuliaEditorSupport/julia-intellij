a1c3f50a3779002a2a57d79b659b709153a97362
ice1000 <ice1000kotlin@foxmail.com>
2018-02-27 21:35:25 +0800

    [ #103 ] Refactor again and again

--- src/org/ice1000/julia/lang/editing/hint/julia-eval-inline.kt
+++ src/org/ice1000/julia/lang/editing/hint/julia-eval-inline.kt
@@ -3,6 +3,7 @@ package org.ice1000.julia.lang.editing.hint
 import com.intellij.codeInsight.hints.*
 import com.intellij.psi.PsiElement
 import org.ice1000.julia.lang.JuliaLanguage
+import org.ice1000.julia.lang.executeJulia
 import org.ice1000.julia.lang.module.juliaSettings
 import org.ice1000.julia.lang.psi.*
 
@@ -27,10 +28,24 @@ enum class JuliaHintType(desc: String, enabled: Boolean) {
 	val option = Option("SHOW_${this.name}", desc, enabled)
 
 	companion object ResolverHolder {
-		fun resolve(elem: PsiElement) = JuliaHintType.values().find { it.isApplicable(elem) }
-		fun resolveToEnabled(elem: PsiElement?): JuliaHintType? {
-			val resolved = elem?.let { resolve(it) } ?: return null
+		private fun resolve(elem: PsiElement) = JuliaHintType.values().find { it.isApplicable(elem) }
+		fun resolveToEnabled(elem: PsiElement): JuliaHintType? {
+			val resolved = resolve(elem) ?: return null
 			return resolved.takeIf { it.option.get() }
+		}
+
+		fun providePropertyTypeHint(elem: JuliaExpr): List<InlayInfo> {
+			val juliaExe = elem.project.juliaSettings.settings.exePath // 这不就是工程的Julia路径吗
+			val output = buildString {
+				val (stdout, stderr) = executeJulia(juliaExe, elem.text, 1000)
+				if (stdout.isNotEmpty()) {
+					append(" => ")
+					stdout.forEach { append(it) }
+				}
+				if (stderr.isNotEmpty()) stderr.forEach { appendln(it) }
+			}
+			val text = if ("print" in elem.text) output.substringBefore("nothing") else output
+			return listOf(InlayInfo(text, elem.textOffset + elem.text.length))
 		}
 	}
 }
