b58359c3d459000017208ddb6cb6de596bfff762
ice1000 <ice1000@sbrella.com>
2018-02-28 19:58:01 +0800

    [ #114 ] Add tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import groovy.lang.Closure
+import org.jetbrains.grammarkit.tasks.GenerateLexer
+import org.jetbrains.grammarkit.tasks.GenerateParser
 import java.io.*
 import java.nio.file.*
 import java.util.stream.Collectors
@@ -46,47 +49,42 @@ allprojects {
 	}
 }
 
-fun collectLines(it: InputStream) =
-	it.bufferedReader().`fuck kotlin! it doesn't support "use" here` {
-		lines().collect(Collectors.toList())
-	}
+java {
+	sourceCompatibility = JavaVersion.VERSION_1_8
+	targetCompatibility = JavaVersion.VERSION_1_8
+}
 
 @Suppress("FunctionName", "ConvertTryFinallyToUseCall")
 @SinceKotlin("1.2")
-fun <Closable, Unit> Closable.`fuck kotlin! it doesn't support "use" here`(block: Closable.() -> Unit): Unit
+inline fun <reified Closable, reified Unit>
+	Closable.`fuck kotlin! it doesn't support "use" here`(block: Closable.() -> Unit): Unit
 	where Closable : Closeable, Unit : Any = try {
 	block()
 } finally {
 	close()
 }
 
-fun executeCommand(
-	command: String,
-	timeLimit: Long = 2000L): List<String> {
-	var processRef: Process? = null
-	var output = emptyList<String>()
-	try {
-		val process: Process = Runtime.getRuntime().exec(command)
-		processRef = process
-		process.waitFor(timeLimit, TimeUnit.MILLISECONDS)
-		output = process.inputStream.`fuck kotlin! it doesn't support "use" here`(::collectLines)
-		process.destroy()
-	} catch (e: Throwable) {
-		processRef?.destroy()
+val commitHash by lazy {
+	val output: String
+	val process: Process = Runtime.getRuntime().exec("git rev-parse --short HEAD")
+	process.waitFor(2000L, TimeUnit.MILLISECONDS)
+	output = process.inputStream.`fuck kotlin! it doesn't support "use" here` {
+		bufferedReader().`fuck kotlin! it doesn't support "use" here` {
+			readText()
+		}
 	}
-	return output
+	process.destroy()
+	output.trim()
 }
 
-val commitHash by lazy {
-	executeCommand("git rev-parse --short HEAD").joinToString("").trim()
-}
+val isCI = !System.getenv("CI").isNullOrBlank()
 
 val pluginVersion = "0.1.6"
 val packageName = "org.ice1000.julia"
 val kotlinVersion: String by extra
 
 group = packageName
-version = if (System.getenv("CI").isNullOrBlank()) pluginVersion else "$pluginVersion-$commitHash"
+version = if (isCI) "$pluginVersion-$commitHash" else pluginVersion
 
 repositories {
 	mavenCentral()
@@ -97,3 +95,66 @@ dependencies {
 	compile(files(Paths.get("lib", "org.eclipse.egit.github.core-2.1.5")))
 	testCompile("junit", "junit", "4.12")
 }
+
+task("displayCommitHash") {
+	group = "help"
+	description = "Display the newest commit hash"
+	doFirst {
+		println("Commit hash: $commitHash")
+	}
+}
+
+task("isCI") {
+	group = "help"
+	description = "Check if it's running in a continuous-integration"
+	doFirst {
+		println(if (isCI) "Yes, I'm on a CI." else "No, I'm not on CI.")
+	}
+}
+
+task("genParser", GenerateParser::class) {
+	group = "build setup"
+	description = "Generate the Parser and PsiElement classes"
+	source = "grammar/julia-grammar.bnf"
+	targetRoot = "gen"
+	pathToParser = "org/ice1000/julia/lang/JuliaParser.java"
+	pathToPsiRoot = "org/ice1000/julia/lang/psi"
+	purgeOldFiles = true
+}
+
+task("genLexer", GenerateLexer::class) {
+	dependsOn("genParser")
+	group = "build setup"
+	description = "Generate the Lexer"
+	source = "grammar/julia-lexer.flex"
+	targetDir = "gen/org/ice1000/julia/lang"
+	targetClass = "JuliaLexer"
+	purgeOldFiles = true
+}
+
+task("genDocfmtParser", GenerateParser::class) {
+	group = "build setup"
+	description = "Generate the Parser for DocumentFormat.jl"
+	source = "grammar/docfmt-grammar.bnf"
+	targetRoot = "gen"
+	pathToParser = "org/ice1000/julia/lang/docfmt/DocfmtParser.java"
+	pathToPsiRoot = "org/ice1000/julia/lang/docfmt/psi"
+	purgeOldFiles = true
+}
+
+task("genDocfmtLexer", GenerateLexer::class) {
+	dependsOn("genDocfmtParser")
+	group = "build setup"
+	description = "Generate the Lexer for DocumentFormat.jl"
+	source = "grammar/docfmt-lexer.flex"
+	targetDir = "gen/org/ice1000/julia/lang/docfmt"
+	targetClass = "DocfmtLexer"
+	purgeOldFiles = true
+}
+
+task("cleanGenerated") {
+
+}
+
+getTasksByName("buildPlugin", false).first().dependsOn("genLexer")
+getTasksByName("buildPlugin", false).first().dependsOn("genDocfmtLexer")
