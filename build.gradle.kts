bda830570c3b63dda0f2b558476322c6bef9de05
ice1000 <ice1000@sbrella.com>
2018-02-28 18:05:37 +0800

    [ kts ] Bugs fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
-import java.io.ByteArrayOutputStream
-import com.google.common.util.concurrent.SimpleTimeLimiter
-import java.io.InputStream
+import java.io.*
 import java.util.stream.Collectors
 
 buildscript {
-	var kotlin_version : String by extra
-	var grammar_kit_version : String by extra
+	var kotlinVersion: String by extra
+	var grammarKitVersion: String by extra
 
-	grammar_kit_version = "2017.1.1"
-	kotlin_version = "1.2.21"
+	grammarKitVersion = "2017.1.1"
+	kotlinVersion = "1.2.21"
 
 	repositories {
 		mavenCentral()
@@ -16,8 +14,8 @@ buildscript {
 	}
 
 	dependencies {
-		classpath(kotlin("gradle-plugin", kotlin_version))
-		classpath("com.github.hurricup:gradle-grammar-kit-plugin:$grammar_kit_version")
+		classpath(kotlin("gradle-plugin", kotlinVersion))
+		classpath("com.github.hurricup:gradle-grammar-kit-plugin:$grammarKitVersion")
 	}
 }
 
@@ -40,10 +38,9 @@ allprojects {
 	intellij {
 		updateSinceUntilBuild = false
 		instrumentCode = true
-		localPath = when(System.getProperty("user.name")) {
+		localPath = when (System.getProperty("user.name")) {
 			"ice1000" -> "/home/ice1000/.local/share/JetBrains/Toolbox/apps/IDEA-U/ch-0/173.4548.28"
 			"hoshino" -> "/home/hoshino/文档/IntelliJ"
-
 			else -> ""
 		}
 	}
@@ -52,61 +49,52 @@ allprojects {
 fun collectLines(it: InputStream): List<String> {
 	val reader = it.bufferedReader()
 	val ret = reader.lines().collect(Collectors.toList())
-	forceRun(reader::close)
+	reader.close()
 	return ret
 }
 
-inline fun forceRun(lambda: () -> Unit) {
-	try {
-		lambda()
-	} catch (e: Throwable) {
-
-	}
+@Suppress("FunctionName", "ConvertTryFinallyToUseCall")
+@SinceKotlin("1.2")
+fun <Closable, Unit> Closable.`fuck kotlin! it doesn't support "use" here`(block: Closable.() -> Unit): Unit
+	where Closable : Closeable, Unit : Any = try {
+	block()
+} finally {
+	close()
 }
 
 fun executeCommand(
 	command: String,
-	input: String?,
-	timeLimit: Long): Pair<List<String>, List<String>> {
+	timeLimit: Long = 2000L): List<String> {
 	var processRef: Process? = null
 	var output: List<String> = emptyList()
-	var outputErr: List<String> = emptyList()
 	try {
-		SimpleTimeLimiter().callWithTimeout({
-			val process: Process = Runtime.getRuntime().exec(command)
-			processRef = process
-			if (input != null) process.outputStream.use {
-				it.write(input.toByteArray())
-				it.flush()
-			}
-			process.waitFor(timeLimit, TimeUnit.MILLISECONDS)
-			output = process.inputStream.also(::collectLines).also { it.close() }
-			outputErr = process.errorStream.use(::collectLines)
-			forceRun(process::destroy)
-		}, timeLimit + 100, TimeUnit.MILLISECONDS, true)
+		val process: Process = Runtime.getRuntime().exec(command)
+		processRef = process
+		process.waitFor(timeLimit, TimeUnit.MILLISECONDS)
+		output = process.inputStream.`fuck kotlin! it doesn't support "use" here`(::collectLines)
+		process.destroy()
 	} catch (e: Throwable) {
 		processRef?.destroy()
 	}
-	return output to outputErr
+	return output
 }
 
-val commitHash get() = ByteArrayOutputStream().apply {
-	//QAQ
-}.toString().trim()
+val commitHash
+	get() = executeCommand("git', 'rev-parse', '--short', 'HEAD").joinToString("").trim()
 
 val pluginVersion = "0.1.6"
 val packageName = "org.ice1000.julia"
-val kotlin_version : String by extra
+val kotlinVersion: String by extra
 
 group = packageName
-version = if(System.getenv("CI")?.trim() != null) "$pluginVersion-$commitHash" else pluginVersion
+version = if (System.getenv("CI").isNullOrBlank()) pluginVersion else "$pluginVersion-$commitHash"
 
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	compile(kotlin("stdlib", kotlin_version))
-	compile("org.eclipse.egit.github.core-2.1.5")
+	compile(kotlin("stdlib", kotlinVersion))
+	compile("", "org.eclipse.egit.github.core-2.1.5")
 	testCompile("junit", "junit", "4.12")
 }
