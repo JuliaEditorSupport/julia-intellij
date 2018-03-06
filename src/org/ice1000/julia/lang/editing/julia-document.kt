75f184dc62f03063522061de62f1f2c9ed4455b1
zxj5470 <zxj5470@foxmail.com>
2018-03-07 01:34:05 +0800

    [ #122 ] MarkDown Imperfect Supported

--- src/org/ice1000/julia/lang/editing/julia-document.kt
+++ src/org/ice1000/julia/lang/editing/julia-document.kt
@@ -1,7 +1,10 @@
 package org.ice1000.julia.lang.editing
 
+import com.github.rjeschke.txtmark.Processor
 import com.intellij.lang.documentation.AbstractDocumentationProvider
 import com.intellij.psi.PsiElement
+import com.intellij.util.PlatformUtils
+import com.petebevin.markdown.MarkdownProcessor
 import org.ice1000.julia.lang.psi.impl.IJuliaFunctionDeclaration
 import org.ice1000.julia.lang.psi.impl.docString
 
@@ -11,7 +14,11 @@ class JuliaDocumentProvider : AbstractDocumentationProvider() {
 	override fun generateDoc(element: PsiElement?, originalElement: PsiElement?): String? {
 		val parent = element?.parent
 		if (parent is IJuliaFunctionDeclaration) {
-			return "function ${parent.toText}\n${parent.docString?.text}"
+			return "function ${parent.toText}\n${parent.docString?.text?.let{
+				//because GoLand has no Markdown4j but MarkdownJ
+				if (PlatformUtils.isGoIde()) MarkdownProcessor().markdown(it)
+				else Processor.process(it)
+			}}"
 		}
 		return "$element,${element?.text}"
 	}
