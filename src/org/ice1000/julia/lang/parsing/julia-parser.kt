dd8a83bc9c5f791902179f67f3c728df5ff987e3
ice1000 <ice1000@sbrella.com>
2018-03-15 17:04:40 +0800

    [ parser ] Too hard...

--- src/org/ice1000/julia/lang/parsing/julia-parser.kt
+++ src/org/ice1000/julia/lang/parsing/julia-parser.kt
@@ -8,6 +8,7 @@ import org.ice1000.julia.lang.JuliaParserDefinition
 /**
  * !!!In progress!!!
  * @author ice1000
+ * @see [org.ice1000.julia.lang.JuliaParser]
  */
 class JuliaParserExperimental : PsiParser, LightPsiParser {
 	override fun parse(root: IElementType, builder: PsiBuilder): ASTNode {
@@ -16,13 +17,29 @@ class JuliaParserExperimental : PsiParser, LightPsiParser {
 	}
 
 	override fun parseLight(root: IElementType, builder: PsiBuilder) {
-		val mark = builder.mark()
-		// TODO 太难了，太难了
-		// TODO 没有金刚钻，别揽瓷器活。。。
+		val mark = builder.markAndAdvance()
+		builder.parse(root)
 		mark.done(root)
+	}
+
+	companion object ParserImplementation {
+		fun PsiBuilder.markAndAdvance() = mark().also { advanceLexer() }
+
+		fun PsiBuilder.parse(root: IElementType) {
+			println(root)
+			while (!eof()) {
+				// TODO 太难了，太难了
+				// TODO 没有金刚钻，别揽瓷器活。。。
+			}
+		}
 	}
 }
 
+/**
+ * !!!In progress!!!
+ * @author ice1000
+ * @see [JuliaParserDefinition]
+ */
 class JuliaParserDefinitionExperimental : JuliaParserDefinition() {
 	override fun createParser(project: Project?) = JuliaParserExperimental()
 }
