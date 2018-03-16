221672e77793a690c3bad3d8443cc9c1941afa9b
ice1000 <ice1000@sbrella.com>
2018-03-16 13:58:04 +0800

    [ #122 ] More

--- src/org/ice1000/julia/lang/editing/julia-document.kt
+++ src/org/ice1000/julia/lang/editing/julia-document.kt
@@ -6,28 +6,26 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.psi.PsiElement
+import com.intellij.ui.content.ContentFactory
 import com.intellij.util.PlatformUtils
 import com.petebevin.markdown.MarkdownProcessor
+import org.ice1000.julia.lang.JuliaBundle
 import org.ice1000.julia.lang.module.ui.JuliaDocumentWindow
-import org.ice1000.julia.lang.psi.impl.IJuliaFunctionDeclaration
-import org.ice1000.julia.lang.psi.impl.docString
-import com.intellij.ui.content.ContentFactory
 import org.ice1000.julia.lang.psi.JuliaSymbol
+import org.ice1000.julia.lang.psi.impl.DocStringOwner
+import org.ice1000.julia.lang.psi.impl.docString
 
 class JuliaDocumentProvider : AbstractDocumentationProvider() {
 	override fun generateDoc(element: PsiElement?, originalElement: PsiElement?): String? {
 		val symbol = element as? JuliaSymbol ?: return null
-		if (symbol.isFunctionName) {
-			val parent = symbol.parent as? IJuliaFunctionDeclaration ?: return null
-			parent.docString?.text?.let {
-				return "function $parent${
-				if (PlatformUtils.isGoIde())
-					MarkdownProcessor().markdown(it)
-				else Processor.process(it)
-				}"
-			}
+		val parent = symbol.parent as? DocStringOwner ?: return null
+		return parent.docString?.text?.let {
+			"function $parent${
+			if (PlatformUtils.isGoIde())
+				MarkdownProcessor().markdown(it)
+			else Processor.process(it)
+			}"
 		}
-		return null
 	}
 }
 
@@ -36,13 +34,13 @@ class JuliaDocumentWindowImpl : JuliaDocumentWindow(), ToolWindowFactory {
 		textPane.text = "Nothing to show"
 	}
 
-	//TODO: change its content like PyCharm Document
-
+	// TODO: change its content like PyCharm Document
 	override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-		val content = ContentFactory
+		toolWindow.contentManager.addContent(ContentFactory
 			.SERVICE
 			.getInstance()
-			.createContent(mainPanel, "", false)
-		toolWindow.contentManager.addContent(content)
+			.createContent(mainPanel,
+				JuliaBundle.message("julia.tool-window.title"),
+				true))
 	}
 }
