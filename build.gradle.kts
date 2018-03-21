94678cedca6d4e640408bf0c13d5f80abad999f0
ice1000 <ice1000@sbrella.com>
2018-03-21 11:46:16 +0800

    [ release ] 0.1.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ val commitHash = kotlin.run {
 	output.trim()
 }
 
-val pluginComingVersion = "0.1.8"
+val pluginComingVersion = "0.1.9"
 val pluginVersion = if (isCI) "$pluginComingVersion-$commitHash" else pluginComingVersion
 val packageName = "org.ice1000.julia"
 val kotlinVersion: String by extra
