b74f1c53dd9b593da0663df5c61cd22f3ccafc3a
ice1000 <ice1000kotlin@foxmail.com>
2018-03-25 17:24:01 +0800

    [ kotlin ] Migrate to 1.2.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ buildscript {
 	var grammarKitVersion: String by extra
 
 	grammarKitVersion = "2018.1.1"
-	kotlinVersion = "1.2.30"
+	kotlinVersion = "1.2.31"
 
 	repositories {
 		mavenCentral()
@@ -57,7 +57,7 @@ plugins {
 	idea
 	java
 	id("org.jetbrains.intellij") version "0.2.18"
-	kotlin("jvm") version "1.2.30"
+	kotlin("jvm") version "1.2.31"
 }
 
 idea {
