b4a1309a39b57487f6b3ca4cce685ee48b656182
zxj5470 <zxj5470@foxmail.com>
2018-09-07 04:04:06 +0800

    [ #198 ] unfinished ( it doesn't work by java.lang.IllegalStateException)

---
+++ src/org/ice1000/julia/lang/editing/julia-posix-template.kt
@@ -0,0 +1,51 @@
+package org.ice1000.julia.lang.editing
+
+import com.intellij.codeInsight.template.postfix.templates.*
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.util.Condition
+import com.intellij.openapi.util.Conditions
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.util.containers.ContainerUtil
+import org.ice1000.julia.lang.editing.JuliaPosixTemplateProvider.Companion.selectorTopmost
+import org.ice1000.julia.lang.psi.JuliaExpr
+
+
+class JuliaPosixTemplateProvider : PostfixTemplateProvider {
+	private val myTemplates: Set<PostfixTemplate>
+
+	companion object {
+		fun selectorTopmost(): PostfixTemplateExpressionSelector {
+			return selectorTopmost(Conditions.alwaysTrue())
+		}
+
+		fun selectorTopmost(additionalFilter: Condition<PsiElement>): PostfixTemplateExpressionSelector {
+			return object : PostfixTemplateExpressionSelectorBase(additionalFilter) {
+				override fun getNonFilteredExpressions(psiElement: PsiElement, document: Document, i: Int): List<PsiElement> {
+					val stat = PsiTreeUtil.getNonStrictParentOfType(psiElement, JuliaExpr::class.java)
+					return ContainerUtil.createMaybeSingletonList(stat?.context)
+				}
+			}
+		}
+	}
+
+	init {
+		myTemplates = ContainerUtil.newHashSet(
+			JuliaPrintPostfixTemplate(this)
+		)
+	}
+
+	override fun getTemplates(): Set<PostfixTemplate> = myTemplates
+	override fun isTerminalSymbol(currentChar: Char) = currentChar == '.'
+	override fun afterExpand(file: PsiFile, editor: Editor) = Unit
+	override fun preCheck(copyFile: PsiFile, realEditor: Editor, currentOffset: Int): PsiFile = copyFile
+	override fun preExpand(file: PsiFile, editor: Editor) = Unit
+}
+
+class JuliaPrintPostfixTemplate(provider: PostfixTemplateProvider) :
+	StringBasedPostfixTemplate("print", "print(expr)", selectorTopmost(), provider) {
+	override fun getTemplateString(psiElement: PsiElement): String? = "print(\$expr\$)"
+	override fun getElementToRemove(expr: PsiElement): PsiElement = expr
+}
