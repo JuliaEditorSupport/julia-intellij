357b765e179934061e13112f3efb8f47518708c9
ice1000 <ice1000kotlin@foxmail.com>
2018-12-01 13:27:46 -0500

    [ release 0.3.4 ] Bump version number

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ val commitHash = kotlin.run {
 	output.trim()
 }
 
-val pluginComingVersion = "0.3.4"
+val pluginComingVersion = "0.3.5"
 val pluginVersion = if (isCI) "$pluginComingVersion-$commitHash" else pluginComingVersion
 val packageName = "org.ice1000.julia"
 val kotlinVersion = "1.2.70"
