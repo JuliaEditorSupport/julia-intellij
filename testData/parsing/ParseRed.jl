432772b0e90a9d4844df7082ab9afa35346108d0
zxj5470 <zxj5470@foxmail.com>
2019-01-22 20:28:42 +0800

    [ #FF0000 ] idkidkwhy

--- testData/parsing/ParseRed.jl
+++ testData/parsing/ParseRed.jl
@@ -3,3 +3,11 @@ cons(R(hd, :<:,r),asd)
 :(<:)
 
 cons(R(hd, :>:,r),asd)
+
+function bnd(out::Set{Any})
+    @match t begin
+        if t isa zxj end => foreach(a,b)
+        if t isa zxj end => push!(emmm)
+        a => @error "emmmm"
+    end
+end
