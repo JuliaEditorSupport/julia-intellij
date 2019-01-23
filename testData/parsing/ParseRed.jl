968ade423d9bdea3e69cb77e353ababc479dce57
zxj5470 <zxj5470@foxmail.com>
2019-01-23 14:34:57 +0800

    [ parser ] elegantly use `functionSignature` in lambda

--- testData/parsing/ParseRed.jl
+++ testData/parsing/ParseRed.jl
@@ -15,3 +15,26 @@ end
 #307
 [1,2,3] * transpose([1,2,4])
 [1,2,3] .* transpose([1,2,4])
+
+function retry(f::Function;  delays=ExponentialBackOff(), check=nothing)
+    (args...; kwargs...) -> begin
+        y = iterate(delays)
+        while y !== nothing
+            (delay, state) = y
+            try
+                return f(args...; kwargs...)
+            catch e
+                y === nothing && rethrow(e)
+                if check !== nothing
+                    result = check(state, e)
+                    state, retry_or_not = length(result) == 2 ? result : (state, result)
+                    retry_or_not || rethrow(e)
+                end
+            end
+            sleep(delay)
+            y = iterate(delays, state)
+        end
+        # When delays is out, just run the function without try/catch
+        return f(args...; kwargs...)
+    end
+end
