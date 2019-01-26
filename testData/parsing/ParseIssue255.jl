db8f91e8efd4bd7ba55a601d2eeed1701e8d07a2
zxj5470 <zxj5470@foxmail.com>
2019-01-26 22:18:58 +0800

    [ #135 #255 ] Build failure is normal.

--- testData/parsing/ParseIssue255.jl
+++ testData/parsing/ParseIssue255.jl
@@ -16,6 +16,29 @@ scaled_ticks = optimize_ticks(sf(amin),sf(amax);
 
 # 3
 const TicksArgs = Union{AVec{T}, Tuple{AVec{T}, AVec{S}}, Symbol} where {T<:Real, S<:AbstractString}
+function differentiate(x::Vector, f::Vector)::Vector
+    length(x) == length(f) ? n = length(x) : throw(DimensionMismatch("The two arguments must have the same length!"))
+
+    derivative = zeros(n)
+    derivative[1] = (f[2] - f[1]) / (x[2] - x[1])
+    for i in 2:(n - 1)
+        derivative[i] = (f[i + 1] - f[i - 1]) / (x[i + 1] - x[i - 1])
+    end
+    derivative[n] = (f[n] - f[n - 1]) / (x[n] - x[n - 1])
+    derivative
+end
+# 4 for expr
+ (xf, xinv), (yf, yinv) = ((scalefunc(s),invscalefunc(s)) for s in (xscale,yscale))
+
+plt[:size] = (s * scale for s in sz)
+
+# 5
+typeof(g)((x_u, (groupedvec2mat(x_ind, x, arg, groupby, NaN) for arg in last_args)...))
+
+# 6. comparable operator in arrayIndex
+st == :pre ? i : i - 1
+# todo
+# newx[idx - 1] = x[st == :pre ? i:(i - 1)]
 
 # 9
 minorticks[amin .<= minorticks .<= amax]
