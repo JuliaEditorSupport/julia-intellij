10082243f6f72338c5743c078ea7bac1f50a995c
ice1000 <ice1000kotlin@foxmail.com>
2019-01-30 21:58:43 -0500

    [ html ] Update change-notes

--- src/org/ice1000/julia/lang/module/julia-package-manager.kt
+++ src/org/ice1000/julia/lang/module/julia-package-manager.kt
@@ -5,6 +5,7 @@ package org.ice1000.julia.lang.module
 
 import org.ice1000.julia.lang.executeCommand
 import org.ice1000.julia.lang.printJulia
+import org.intellij.lang.annotations.Language
 import java.io.File
 import java.nio.file.*
 import java.util.function.Predicate
@@ -18,6 +19,7 @@ data class InfoData(val name: String, val version: String, val latestVersion: St
  *         because the first param will be `let(::File)`'s dir
  */
 fun packageNamesList(importPathNullable: String? = null): Stream<String> {
+	@Language("Julia")
 	val importPath = importPathNullable
 		?: printJulia(juliaPath, timeLimit = 5000L, expr = "Pkg.dir()")
 			.first
@@ -43,6 +45,7 @@ private val packagePredicate = Predicate { dir: Path ->
 /**
  * very slow.
  */
+@Language("Julia")
 fun versionsList(settings: JuliaSettings) =
 	executeCommand(settings.exePath, "Pkg.installed()", 20_000L)
 		.first
@@ -62,6 +65,7 @@ fun loadNamesListByEnvFile(settings: JuliaSettings, envdir: String): List<String
 }
 
 fun getEnvDir(settings: JuliaSettings): String {
+	//language=Julia
 	return executeCommand(settings.exePath, "using Pkg\nPkg.envdir()\nexit()", timeLimit = 4000L)
 		.first
 		.firstOrNull()
