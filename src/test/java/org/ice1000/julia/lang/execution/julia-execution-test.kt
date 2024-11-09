76b9153ad462453a781005cc716fcc3a2f81fc38
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2024-11-09 17:29:29 +0400

    #1835 Fixed tests running
    Tests for the plugin are basically do not exist. Only parsing covered in a way.

--- src/test/java/org/ice1000/julia/lang/execution/julia-execution-test.kt
+++ src/test/java/org/ice1000/julia/lang/execution/julia-execution-test.kt
@@ -1,3 +1,21 @@
+/*
+ *     Julia language support plugin for Intellij-based IDEs.
+ *     Copyright (C) 2024 julia-intellij contributors
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.ice1000.julia.lang.execution
 
 import com.google.common.io.Files
@@ -12,7 +30,6 @@ import java.io.File
 import java.io.IOException
 import java.util.concurrent.TimeUnit
 import kotlin.system.measureTimeMillis
-import kotlin.test.assertTrue
 
 private fun main(args: Array<String>) {
 	val process = Runtime.getRuntime().exec("$juliaPath -q").also {
