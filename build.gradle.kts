37500571ed094be5243882b78da4cbda43514e7a
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2024-11-09 11:37:02 +0400

    #1835 Build script update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,192 +1,315 @@
-@file:kotlin.Suppress("unsupported")
-import org.jetbrains.grammarkit.tasks.GenerateLexer
-import org.jetbrains.grammarkit.tasks.GenerateParser
-import org.jetbrains.intellij.tasks.PatchPluginXmlTask
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.jetbrains.grammarkit.tasks.GenerateLexerTask
+import org.jetbrains.grammarkit.tasks.GenerateParserTask
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.io.*
+import org.kt3k.gradle.plugin.coveralls.CoverallsTask
 import java.nio.file.Paths
 
-val isCI = !System.getenv("CI").isNullOrBlank()
-val commitHash = kotlin.run {
-	val process: Process = Runtime.getRuntime().exec("git rev-parse --short HEAD")
-	process.waitFor()
-	val output = process.inputStream.use {
-		process.inputStream.use { it.readBytes().let(::String) }
-	}
-	process.destroy()
-	output.trim()
-}
-
-val pluginComingVersion = "0.4.2"
-val pluginVersion = if (isCI) "$pluginComingVersion-$commitHash" else pluginComingVersion
-val packageName = "org.ice1000.julia"
-
-group = packageName
-version = pluginVersion
+fun properties(key: String) = providers.gradleProperty(key)
+fun environment(key: String) = providers.environmentVariable(key)
+val isCI = environment("CI").map { it.toBoolean() }.orElse(false)
+val withCoverage =
+	environment("COVERALLS_REPO_TOKEN").orElse(properties("with_coverage")).map { !it.isEmpty() }.orElse(false)
+val platformVersionProvider by extra(project.provider {
+	properties("platformVersion").get() + properties("platformBranch").get() + properties("platformBuild").get()
+})
 
 plugins {
-	java
-	id("org.jetbrains.intellij") version "0.7.2"
-	id("org.jetbrains.grammarkit") version "2020.3.2"
-	kotlin("jvm") version "1.3.60"
-}
-
-fun fromToolbox(root: String, ide: String) = file(root)
-	.resolve(ide)
-	.takeIf { it.exists() }
-	?.resolve("ch-0")
-	?.listFiles()
-	.orEmpty()
-	.filterNotNull()
-	.filter { it.isDirectory }
-	.filterNot { it.name.endsWith(".plugins") }
-	.maxBy {
-		val (major, minor, patch) = it.name.split('.')
-		String.format("%5s%5s%5s", major, minor, patch)
-	}
-	?.also { println("Picked: $it") }
-
-allprojects {
-	apply { plugin("org.jetbrains.grammarkit") }
-}
-
-//grammarKit {
-//	grammarKitRelease = "7aecfcd72619e9c241866578e8312f339b4ddbd8"
-//}
-
-intellij {
-	updateSinceUntilBuild = false
-	instrumentCode = true
-	if (!isCI) {
-		setPlugins("PsiViewer:203-SNAPSHOT", "java")
-		downloadSources = true
-	} else {
-		setPlugins("java")
-		version = "2020.3"
-	}
-	val user = System.getProperty("user.name")
-	val os = System.getProperty("os.name")
-	val root = when {
-		os.startsWith("Windows") -> "C:\\Users\\$user\\AppData\\Local\\JetBrains\\Toolbox\\apps"
-		os == "Linux" -> "/home/$user/.local/share/JetBrains/Toolbox/apps"
-		else -> return@intellij
-	}
-	val intellijPath = ["IDEA-C", "IDEA-U"]
-		.mapNotNull { fromToolbox(root, it) }.firstOrNull()
-	intellijPath?.absolutePath?.let { localPath = it }
-	val pycharmPath = ["PyCharm-C", "IDEA-C", "IDEA-U"]
-		.mapNotNull { fromToolbox(root, it) }.firstOrNull()
-	pycharmPath?.absolutePath?.let { alternativeIdePath = it }
-}
-
-java {
-	sourceCompatibility = JavaVersion.VERSION_11
-	targetCompatibility = JavaVersion.VERSION_11
-}
-
-tasks.withType<PatchPluginXmlTask>().configureEach {
-	changeNotes(file("docs/change-notes.html").readText())
-	pluginDescription(file("docs/description.html").readText())
-	version(pluginVersion)
-	pluginId(packageName)
-}
-
-sourceSets {
-	main {
-		withConvention(KotlinSourceSet::class) {
-			listOf(java, kotlin).forEach { it.srcDirs("src", "gen") }
-		}
-		resources.srcDir("res")
-	}
-
-	test {
-		withConvention(KotlinSourceSet::class) {
-			listOf(java, kotlin).forEach { it.srcDirs("test") }
-		}
-		resources.srcDir("testData")
-	}
+	id("idea")
+	id("jacoco")
+	id("org.jetbrains.intellij.platform") version "2.0.1"
+	id("org.jetbrains.grammarkit") version "2022.3.2.2"
+	id("com.github.kt3k.coveralls") version "2.12.2"
+	id("org.sonarqube") version "5.1.0.4882"
+	id("org.jetbrains.qodana") version "0.1.13"
+	id("org.jetbrains.kotlin.jvm") version "2.0.21"
 }
 
 repositories {
 	mavenCentral()
-	maven("https://dl.bintray.com/jetbrains/markdown/")
+}
+
+val pluginProjectsNames = setOf<String>()
+
+allprojects {
+	val isPlugin = project.name in pluginProjectsNames || true // this is for further usage of modularization
+	apply(plugin = "org.jetbrains.grammarkit")
+	apply(plugin = if (isPlugin) "org.jetbrains.intellij.platform" else "org.jetbrains.intellij.platform.module")
+	apply(plugin = "com.github.kt3k.coveralls")
+	apply(plugin = "jacoco")
+	apply(plugin = "java")
+	apply(plugin = "org.jetbrains.kotlin.jvm")
+
+	repositories {
+		mavenCentral()
+		intellijPlatform {
+			defaultRepositories()
+			jetbrainsRuntime()
+		}
+	}
+
+	grammarKit {
+		jflexRelease.set(properties("jflexVersion"))
+	}
+
+	version = properties("pluginVersion").get().ifEmpty { properties("platformVersion").get() } +
+		properties("pluginBranch").get().ifEmpty { properties("platformBranch").get() } +
+		properties("pluginBuild").get().ifEmpty { properties("platformBuild").get() }
+
+
+	dependencies {
+		intellijPlatform {
+			instrumentationTools()
+			testFramework(TestFrameworkType.Platform)
+			jetbrainsRuntime()
+		}
+		testImplementation("org.opentest4j:opentest4j:1.3.0")
+	}
+
+	tasks {
+		withType<JavaCompile> {
+			options.encoding = "UTF-8"
+			sourceCompatibility = properties("javaVersion").get()
+			targetCompatibility = properties("javaTargetVersion").get()
+		}
+
+		withType<KotlinCompile> {
+			kotlinOptions.jvmTarget = properties("javaTargetVersion").get()
+		}
+
+		test {
+			maxHeapSize = "2048m"
+			outputs.upToDateWhen { false }
+
+			if (project.hasProperty("overwrite")) {
+				systemProperty("idea.tests.overwrite.data", "true")
+			}
+
+			if (project.hasProperty("youtrack.token")) {
+				systemProperty("youtrack.token", properties("youtrack.token").get())
+			}
+
+			if (project.hasProperty("idea.split.test.logs")) {
+				systemProperty("idea.split.test.logs", "true")
+				systemProperty("idea.single.test.log.max.length", "100_000_000")
+			}
+
+			useJUnit {
+				if (project.hasProperty("runtest")) {
+					include("**/" + properties("runtest").get() + ".class")
+				}
+
+				if (project.hasProperty("integrationTests")) {
+					excludeCategories("categories.Light")
+					excludeCategories("categories.Heavy")
+				} else if (project.hasProperty("heavyTests")) {
+					excludeCategories("categories.Light")
+					excludeCategories("categories.Integration")
+				} else if (project.hasProperty("lightTests")) {
+					excludeCategories("categories.Heavy")
+					excludeCategories("categories.Integration")
+				} else if (!project.hasProperty("allTests")) {
+					excludeCategories("categories.Integration")
+				}
+			}
+
+			testLogging {
+				exceptionFormat = TestExceptionFormat.FULL
+				showStandardStreams = true
+			}
+
+			configure<JacocoTaskExtension> {
+				isEnabled = withCoverage.get()
+				isIncludeNoLocationClasses = true
+				excludes = listOf("jdk.internal.*")
+			}
+
+			if (isCI.get()) {
+				testLogging {
+					events.addAll(
+						listOf(
+							TestLogEvent.PASSED,
+							TestLogEvent.SKIPPED,
+							TestLogEvent.FAILED,
+							TestLogEvent.STANDARD_OUT,
+							TestLogEvent.STANDARD_ERROR
+						)
+					)
+					exceptionFormat = TestExceptionFormat.FULL
+				}
+			}
+		}
+
+		if (isPlugin) {
+			publishPlugin {
+				if (project.hasProperty("eap")) {
+					channels.set(listOf("EAP"))
+				}
+				token.set(properties("jbToken").orElse(""))
+			}
+
+			patchPluginXml {
+				pluginDescription.set(properties("descriptionFile").flatMap {
+					providers.fileContents(layout.projectDirectory.file(it)).asText
+				})
+
+				changeNotes.set(properties("changesFile").flatMap {
+					providers.fileContents(layout.projectDirectory.file(it)).asText
+				})
+			}
+		}
+	}
+}
+
+tasks {
+	val jacocoRootReport = register<JacocoReport>("jacocoRootReport") {
+		group = "verification"
+		description = "Generates an aggregate report from all projects"
+
+		dependsOn(allprojects.map {
+			it.tasks.named("jacocoTestReport").map { task -> task.dependsOn }
+		})
+		mustRunAfter(allprojects.map {
+			it.tasks.named("jacocoTestReport").map { task -> task.mustRunAfter }
+		})
+		executionData(allprojects.map {
+			it.tasks.named<JacocoReport>("jacocoTestReport").map { task -> task.executionData }
+		})
+
+		executionData(File("coverage").walkTopDown().filter { it.extension == "exec" }.toList())
+
+		additionalSourceDirs.setFrom(allprojects.map {
+			it.sourceSets.main.map { sourceSet -> sourceSet.allSource.srcDirs }
+		})
+		sourceDirectories.setFrom(allprojects.map {
+			it.sourceSets.main.map { sourceSet -> sourceSet.allSource.srcDirs }
+		})
+
+		reports {
+			html.required.set(true) // human readable
+			xml.required.set(true) // required by coveralls
+			csv.required.set(false)
+		}
+	}
+
+	withType<CoverallsTask> {
+		group = "verification"
+		description = "Uploads the aggregated coverage report to Coveralls"
+		dependsOn(jacocoRootReport)
+	}
+
+	register("generateLexers") { }
+
+	runIde {
+		project.properties.forEach { (key, value) ->
+			if (key.startsWith("pass.")) {
+				val passedKey = key.substring(5)
+				println("Passing $passedKey => $value")
+				systemProperty(passedKey, value.toString())
+			}
+		}
+
+		jvmArgs("-Xmx2048m")
+	}
+}
+
+val coverageReportFile = project.buildDir.resolve("reports/jacoco/jacocoRootReport/jacocoRootReport.xml")
+
+sourceSets {
+	main {
+			listOf(java, kotlin).forEach { it.srcDirs("src", "gen") }
+		resources.srcDir("res")
+	}
+
+	test {
+			listOf(java, kotlin).forEach { it.srcDirs("test") }
+		resources.srcDir("testData")
+	}
 }
 
 dependencies {
-	compile(kotlin("stdlib-jdk8"))
+/*
 	compile(group = "org.eclipse.mylyn.github", name = "org.eclipse.egit.github.core", version = "2.1.5") {
 		exclude(module = "gson")
 	}
 	compile("org.jetbrains", "markdown", "0.2.0")
-	testCompile(kotlin(module = "test-junit"))
-	testCompile(group = "junit", name = "junit", version = "4.12")
+*/
 }
 
-tasks.register("displayCommitHash") {
-	group = "help"
-	description = "Display the newest commit hash"
-	doFirst { println("Commit hash: $commitHash") }
-}
+intellijPlatform {
+	val pluginList = mutableListOf<String>()
+	val bundledPluginList = mutableListOf<String>()
 
-tasks.register("isCI") {
-	group = "help"
-	description = "Check if it's running in a continuous-integration"
-	doFirst { println(if (isCI) "Yes, I'm on a CI." else "No, I'm not on CI.") }
+	if (!isCI.get()) {
+		pluginList.add("PsiViewer:${properties("psiViewerVersion").get()}")
+	}
+
+	val runWith = properties("runWith").orElse("")
+	val (ideType, ideVersion) = when (runWith.get()) {
+		"CL" -> {
+			"CL" to properties("clionVersion").get()
+		}
+
+		"PC" -> {
+			"PC" to properties("pycharmVersion").get()
+		}
+
+		"PY" -> {
+			"PY" to properties("pycharmVersion").get()
+		}
+
+		else -> {
+			"IC" to platformVersionProvider.get()
+		}
+	}
+	dependencies {
+		intellijPlatform {
+			create(ideType, ideVersion, useInstaller = properties("useInstaller").get().toBoolean())
+			plugins(pluginList)
+			bundledPlugins(bundledPluginList)
+		}
+	}
 }
 
 // Don't specify type explicitly. Will be incorrectly recognized
-val parserRoot = Paths.get("org", "ice1000", "julia", "lang")
-val lexerRoot = Paths.get("gen", "org", "ice1000", "julia", "lang")
-fun path(more: Iterable<*>) = more.joinToString(File.separator)
-fun bnf(name: String) = Paths.get("grammar", "$name-grammar.bnf").toString()
-fun flex(name: String) = Paths.get("grammar", "$name-lexer.flex").toString()
+fun bnf(name: String) = Paths.get("grammar", "$name-grammar.bnf").toFile()
+fun flex(name: String) = Paths.get("grammar", "$name-lexer.flex").toFile()
 
-val genParser = tasks.register<GenerateParser>("genParser") {
+val genParser = tasks.register<GenerateParserTask>("genParser") {
 	group = "code generation"
 	description = "Generate the Parser and PsiElement classes"
-	source = bnf("julia")
-	targetRoot = "gen/"
-	pathToParser = path(parserRoot + "JuliaParser.java")
-	pathToPsiRoot = path(parserRoot + "psi")
-	purgeOldFiles = true
+	sourceFile.set(bnf("julia"))
+	pathToParser.set("/org/ice1000/julia/lang/JuliaParser.java")
+	pathToPsiRoot.set("/org/ice1000/julia/lang/psi")
+	targetRootOutputDir.set(file("gen"))
 }
 
-val genLexer = tasks.register<GenerateLexer>("genLexer") {
+val genLexer = tasks.register<GenerateLexerTask>("genLexer") {
 	group = "code generation"
 	description = "Generate the Lexer"
-	source = flex("julia")
-	targetDir = path(lexerRoot)
-	targetClass = "JuliaLexer"
-	purgeOldFiles = true
+	sourceFile.set(flex("julia"))
+	targetOutputDir.set(file("gen/org/ice1000/julia/lang"))
+
 	dependsOn(genParser)
 }
 
-val genDocfmtParser = tasks.register<GenerateParser>("genDocfmtParser") {
+val genDocfmtParser = tasks.register<GenerateParserTask>("genDocfmtParser") {
 	group = "code generation"
 	description = "Generate the Parser for DocumentFormat.jl"
-	source = bnf("docfmt")
-	targetRoot = "gen/"
-	val root = parserRoot + "docfmt"
-	pathToParser = path(root + "DocfmtParser.java")
-	pathToPsiRoot = path(root + "psi")
-	purgeOldFiles = true
+	sourceFile.set(bnf("docfmt"))
+	pathToParser.set("/org/ice1000/julia/lang/docfmt/DocfmtParser.java")
+	pathToPsiRoot.set("/org/ice1000/julia/lang/docfmt/psi")
+	targetRootOutputDir.set(file("gen"))
 }
 
-val genDocfmtLexer = tasks.register<GenerateLexer>("genDocfmtLexer") {
+val genDocfmtLexer = tasks.register<GenerateLexerTask>("genDocfmtLexer") {
 	group = "code generation"
 	description = "Generate the Lexer for DocumentFormat.jl"
-	source = flex("docfmt")
-	targetDir = path(lexerRoot + "docfmt")
-	targetClass = "DocfmtLexer"
-	purgeOldFiles = true
+	sourceFile.set(flex("docfmt"))
+	targetOutputDir.set(file(file("gen/org/ice1000/julia/lang/docfmt")))
 	dependsOn(genDocfmtParser)
-}
-
-val cleanGenerated = tasks.register("cleanGenerated") {
-	group = "clean"
-	description = "Remove all generated codes"
-	doFirst { delete("gen") }
 }
 
 val sortSpelling = tasks.register("sortSpellingFile") {
@@ -203,18 +326,8 @@ val sortSpelling = tasks.register("sortSpellingFile") {
 
 tasks.withType<KotlinCompile>().configureEach {
 	dependsOn(
-		genParser,
 		genLexer,
-		genDocfmtParser,
 		genDocfmtLexer,
 		sortSpelling
 	)
-	kotlinOptions {
-		jvmTarget = "1.8"
-		languageVersion = "1.3"
-		apiVersion = "1.3"
-		freeCompilerArgs = listOf("-Xjvm-default=enable")
-	}
 }
-
-tasks.withType<Delete>().configureEach { dependsOn(cleanGenerated) }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,8 @@
+pluginManagement {
+	repositories {
+		maven("https://oss.sonatype.org/content/repositories/snapshots/")
+		gradlePluginPortal()
+	}
+}
 rootProject.name = "julia-intellij"
 
