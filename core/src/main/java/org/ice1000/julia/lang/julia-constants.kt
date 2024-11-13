ca436d20fc4083289ea44dac19cf41f920216fc9
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2024-11-13 19:41:49 +0400

    #1835 Moved context id to the xml file

--- core/src/main/java/org/ice1000/julia/lang/julia-constants.kt
+++ core/src/main/java/org/ice1000/julia/lang/julia-constants.kt
@@ -1,3 +1,21 @@
+/*
+ *     Julia language support plugin for Intellij-based IDEs.
+ *     Copyright (C) 2024 julia-intellij contributors
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.ice1000.julia.lang
 
 import com.intellij.execution.process.OSProcessHandler
@@ -8,7 +26,6 @@ import org.ice1000.julia.lang.module.JuliaVariablesView
 import org.intellij.lang.annotations.Language
 import org.jetbrains.annotations.NonNls
 
-@NonNls const val JULIA_CONTEXT_ID = "JULIA_CONTEXT"
 @NonNls const val JULIA_LANGUAGE_NAME = "Julia"
 @NonNls const val JULIA_EXTENSION = "jl"
 @NonNls const val JULIA_DOC_SURROUNDING = "\"\"\""
--- core/src/main/java/org/ice1000/julia/lang/julia-infos.kt
+++ core/src/main/java/org/ice1000/julia/lang/julia-infos.kt
@@ -1,10 +1,31 @@
+/*
+ *     Julia language support plugin for Intellij-based IDEs.
+ *     Copyright (C) 2024 julia-intellij contributors
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.ice1000.julia.lang
 
 import com.intellij.CommonBundle
 import com.intellij.codeInsight.template.TemplateContextType
 import com.intellij.codeInsight.template.impl.DefaultLiveTemplatesProvider
 import com.intellij.extapi.psi.PsiFileBase
-import com.intellij.openapi.fileTypes.*
+import com.intellij.openapi.fileTypes.ExactFileNameMatcher
+import com.intellij.openapi.fileTypes.FileTypeConsumer
+import com.intellij.openapi.fileTypes.FileTypeFactory
+import com.intellij.openapi.fileTypes.LanguageFileType
 import com.intellij.psi.FileViewProvider
 import com.intellij.psi.PsiFile
 import icons.JuliaIcons
@@ -31,7 +52,7 @@ class JuliaFileTypeFactory : FileTypeFactory() {
 	}
 }
 
-class JuliaContext : TemplateContextType(JULIA_CONTEXT_ID, JULIA_LANGUAGE_NAME) {
+class JuliaContext : TemplateContextType(JULIA_LANGUAGE_NAME) {
 	override fun isInContext(file: PsiFile, offset: Int) = file.fileType == JuliaFileType
 }
 
